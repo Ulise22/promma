458aff7f784f80595872140554ab24f28d711294
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-10-20 14:36:05 -0300

    fixed some error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel='icon' href='/PROMMA.svg' />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9270621638584149"
-     crossOrigin="anonymous"></script>
+     crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
