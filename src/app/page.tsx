d8ed5fa6f18fc0b7a29168f9f0da9cd1cc30f101
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-10-20 16:28:16 -0300

    new articles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 import styles from './page.module.css'
 import mmadescription from '../assets/mma-description.jpg'
+import emisordi from '../assets/Emiliano_Sordi.jpg'
 import Image from 'next/image'
 
 export default function Home () {
@@ -17,6 +18,25 @@ export default function Home () {
           <p className={styles.article__p}>Es un deporte relativamente nuevo, dado que comenzó a popularizarse y a conocerse como tal en los años 90, cuando se organizaron en Estados Unidos y Japón los primeros torneos de esta disciplina.</p>
           <Image className={styles.article__img} src={mmadescription} alt='2 peladores luchando' />
           <p className={styles.article__p}>Su creciente popularidad en los últimos años, dió lugar a la creación de diferentes empresas y organizaciones de renombre como lo son la UFC (Ultimate Fighting Championship), o Bellator MMA, entre otras. Estas organizaciones organizan eventos donde los luchadores compiten en un octágono o un ring, y las peleas son transmitidas por televisión, con una cada vez mayor audiencia.</p>
+        </article>
+        <article>
+          <h2 className={styles.article__title}>Tipos de Artes Marciales</h2>
+          <p className={styles.article__p}>Existen diferentes tipos de artes marciales que se utilizan en las mma, aunque los más utilizados son los siguientes: </p>
+          <ul>
+            <li>Boxeo</li>
+            <li>Jiu-jitsu</li>
+            <li>Karate</li>
+            <li>Kickboxing</li>
+            <li>Taekwondo</li>
+            <li>Muay Thai</li>
+            <li>Lucha Libre</li>
+            <li>Judo</li>
+          </ul>
+        </article>
+        <article className={styles.article}>
+          <h2 className={styles.article__title}>Mma argentina</h2>
+          <p className={styles.article__p}>Las MMA en Argentina vienen creciendo como en todo el mundo, llegando a tener un peleador campeón de la Professional Fighthers League(PFL) como <b>Emiliano Sordi</b> o al capeón de peso welter en la compañía ARES como <b>Laureano Staropoli</b>. Además claro, de nuestros representantes en la UFC, como lo son <b>Santiago Ponzibbio, Guido Cannetti y Marcelo Rojo</b> en la rama masculina, junto a <b>Silvana Gómez y Ailín Pérez</b> en la rama femenina.</p>
+          <Image className={styles.article__img} src={emisordi} alt='Emiliano Sordi campeon argentino de mma' width={500} />
         </article>
       </section>
     </main>
