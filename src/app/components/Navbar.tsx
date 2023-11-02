e2a9a8aa3e433bf9f6e0ca4ca55b519e96058bda
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-02 12:17:26 -0300

    made the navbar component responsive

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,36 @@
+'use client'
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from './Navbar.module.css'
 import logo from '../../assets/PROMMA.svg'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar () {
+    const [isOpen, setIsOpen] = useState(false)
     return(
         <nav className={styles.navbar}>
-            <Link className={styles.navbar__link} href='/peleadores'>Peleadores</Link>
-            <Link href='/'>
-                <Image className={styles.navbar__logo} src={logo} alt='promma mma' />
+            <Link className={styles.logo} href='/'>
+                <Image src={logo} alt='promma mma' />
             </Link>
-            <Link className={styles.navbar__link} href='/estilos'>Estilos</Link>
+
+            <div onClick={() => setIsOpen(!isOpen)} className={styles.openMenu}>
+                { isOpen ? <FontAwesomeIcon icon={faXmark} size='xl' /> : <FontAwesomeIcon icon={faBars} size='xl' /> } 
+            </div>
+            <section className={isOpen ? `${styles.navbar__container} ${styles.active}` : `${styles.navbar__container}`}>
+                <div>
+                    <Link className={styles.navbar__link} href='/'>Inicio</Link>
+                    <Link className={styles.navbar__link} href='/peleadores'>Peleadores</Link>
+                </div>
+                <Link href='/'>
+                    <Image className={styles.navbar__logo} src={logo} alt='promma mma' />
+                </Link>
+                <div>
+                    <Link className={styles.navbar__link} href='/estilos'>Estilos</Link>
+                    <Link className={styles.navbar__link} href='/contacto'>Contacto</Link>
+                </div>
+            </section>
         </nav>
     )
 }
