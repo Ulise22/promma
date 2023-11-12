01e41c603e2b0e4f8715fc0d660f167f1f2475fe
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-12 18:42:00 -0300

    made the peleadores styles reusable

--- src/app/peleadores/(peleadores)/emiliano-sordi/page.tsx
+++ src/app/peleadores/(peleadores)/emiliano-sordi/page.tsx
@@ -1,35 +1,34 @@
 import Image from "next/image";
 import emiliano from '../../../../assets/peleadores__images/emiliano-sordi/emiliano.png'
 import sordi from '../../../../assets/peleadores__images/emiliano-sordi/emiliano-sordi.jpg'
-import styles from './Emiliano.module.css'
+import styles from '../../components/peleador.module.css'
 import PeleadoresHero from "../../components/PeleadoresHero";
 
 export default function Emiliano () {
     return(
         <main>
             <PeleadoresHero peleadoresImage={emiliano} w={23} l={10} d={1} nombre='Emiliano Sordi' categoria='Peso semi-pesado (Light Heavyweight)' apodo='He Man' />
-            <section className={styles.emiliano__articles}>
-                <article>
-                    <h2 className={styles.emiliano__title}>Emiliano Sordi mma</h2>
-                    <p className={styles.emiliano__text}>Emiliano Sordi es un peleador de mma argentino, que ha llegado a competir en diversas compañías como Bellator y PFL, que destacó en 2019 al consagrarse campeón mundial de los semicompletos en la compañía PFL (Professional Fighters League).</p>
-                    <p className={styles.emiliano__text}>El cordobés, logró en diciembre de 2019 consagrarse en el Madison Square Garden de Nueva York al vencer por la vía del nocaut al americano Jordan Johnson.</p>
+            <section className={styles.peleador__articles__container}>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Emiliano Sordi mma</h2>
+                    <p className={styles.peleador__article__text}>Emiliano Sordi es un peleador de mma argentino, que ha llegado a competir en diversas compañías como Bellator y PFL, que destacó en 2019 al consagrarse campeón mundial de los semicompletos en la compañía PFL (Professional Fighters League).</p>
+                    <p className={styles.peleador__article__text}>El cordobés, logró en diciembre de 2019 consagrarse en el Madison Square Garden de Nueva York al vencer por la vía del nocaut al americano Jordan Johnson.</p>
                 </article>
-                <article>
-                    <h2 className={styles.emiliano__title}>Emiliano Sordi PFL</h2>
-                    <p className={styles.emiliano__text}>El 30 de agosto de 2018, Emiliano hizo su debut en PFL con un sorprendente ko a los 16 segundos del primer asalto contra el estadounidense Jason Butcher, desde entonces luchó un total de 12 veces, de las cuales ganó 7, perdió 4 y empató 1. </p>
-                    <Image className={styles.emiliano__article__image} src={sordi} alt="Emiliano Sordi saludando a su rival de MMA en un combate para la compañía PFL en 2021" />
-                    <p className={styles.emiliano__text}>Cabe destacar, que a diferencia de otras compañías como UFC, PFL tiene un formato diferente, donde no hay que defender el titulo ganado, ni arrebatarselo al vigente campeón. En su lugar, se entrega un titulo por año. En este sistema es donde Emiliano Sordi supo hacerse con el campeonato mundial de los semipesados de PFL en 2019, menos de un año y medio después de su debut.</p>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Emiliano Sordi PFL</h2>
+                    <p className={styles.peleador__article__text}>El 30 de agosto de 2018, Emiliano hizo su debut en PFL con un sorprendente ko a los 16 segundos del primer asalto contra el estadounidense Jason Butcher, desde entonces luchó un total de 12 veces, de las cuales ganó 7, perdió 4 y empató 1. </p>
+                    <Image className={styles.peleador__article__image} src={sordi} alt="Emiliano Sordi saludando a su rival de MMA en un combate para la compañía PFL en 2021" />
+                    <p className={styles.peleador__article__text}>Cabe destacar, que a diferencia de otras compañías como UFC, PFL tiene un formato diferente, donde no hay que defender el titulo ganado, ni arrebatarselo al vigente campeón. En su lugar, se entrega un titulo por año. En este sistema es donde Emiliano Sordi supo hacerse con el campeonato mundial de los semipesados de PFL en 2019, menos de un año y medio después de su debut.</p>
                 </article>
-                <article>
-                    <h2 className={styles.emiliano__title}>Emiliano Sordi ufc</h2>
-                    <p className={styles.emiliano__text}>Emiliano en una entrevista reconoció luego de consagrarse campeón de los semicompletos de PFL, recibió varias propuestas para pelear en la UFC. Sin embargo dijo sentirse cómodo en PFL, ya que no busca publicidad y prefiere mantenerse en el anonimato.</p>
-                    <p className={styles.emiliano__text}>Además no se detuvo a la hora de dar elogios a su actual compañía, en la que actualmente sigue peleando.</p>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Emiliano Sordi ufc</h2>
+                    <p className={styles.peleador__article__text}>Emiliano en una entrevista reconoció luego de consagrarse campeón de los semicompletos de PFL, recibió varias propuestas para pelear en la UFC. Sin embargo dijo sentirse cómodo en PFL, ya que no busca publicidad y prefiere mantenerse en el anonimato. Además no se detuvo a la hora de dar elogios a su actual compañía, en la que actualmente sigue peleando.</p>
                 </article>
-                <article className={styles.emiliano__article__container}>
-                    <h2 className={styles.emiliano__title}>Última pelea de Emiliano Sordi</h2>
-                    <p className={styles.emiliano__text}>En junio del 2022, Emiliano volvió a pelear en lo que es hasta ahora, su último combate en la compañía PFL contra al brasileño Delan Monte, que venía con un record de 8 victorias y 2 derrotas (ganando 5 por knocaouts y 3 por sumisión).</p>
-                    <p className={styles.emiliano__text}>Lamentablemente para el argentino, el combate terminó en una derrota por TKO (nocaut técnico) en el primer asalto para Emiliano Sordi y en una victoria para el peleador brasileño.</p>
-                    <iframe className={styles.emiliano__video} width="560" height="315" src="https://www.youtube.com/embed/Fgf6eQT6X8k?si=JRzSXaklxbY3IYnt" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Última pelea de Emiliano Sordi</h2>
+                    <p className={styles.peleador__article__text}>En junio del 2022, Emiliano volvió a pelear en lo que es hasta ahora, su último combate en la compañía PFL contra al brasileño Delan Monte, que venía con un record de 8 victorias y 2 derrotas (ganando 5 por knocaouts y 3 por sumisión).</p>
+                    <p className={styles.peleador__article__text}>Lamentablemente para el argentino, el combate terminó en una derrota por TKO (nocaut técnico) en el primer asalto para Emiliano Sordi y en una victoria para el peleador brasileño.</p>
+                    <iframe className={styles.peleador__article__video} width="560" height="315" src="https://www.youtube.com/embed/Fgf6eQT6X8k?si=JRzSXaklxbY3IYnt" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </article>
             </section>
         </main>
--- src/app/peleadores/(peleadores)/illia-topuria/page.tsx
+++ src/app/peleadores/(peleadores)/illia-topuria/page.tsx
@@ -1,32 +1,32 @@
 import Image from "next/image";
 import topuria from '../../../../assets/peleadores__images/ilia-topuria/topuria.png'
 import ilia from '../../../../assets/peleadores__images/ilia-topuria/ilia-topuria.jpg'
-import styles from './Illia.module.css'
+import styles from '../../components/peleador.module.css'
 import PeleadoresHero from "../../components/PeleadoresHero";
 
 export default function Illia () {
     return(
         <main>
             <PeleadoresHero peleadoresImage={topuria} w={14} l={0} d={0} nombre='Ilia Topuria' categoria='Peso pluma (featherweight)' apodo='El Matador' />
-            <section className={styles.ilia__articles}>
-                <article className={styles.ilia__article__container}>
-                    <h2 className={styles.ilia__title}>Ilia Topuria ufc</h2>
-                    <p className={styles.ilia__text}>Ilia Topuria es un luchador de nacionalidad georgiana y española de ufc que compite en la categoría de Peso Pluma. Pelea a nivel profesional como luchador de MMA desde 2015, cuando comenzó a competir en promotoras locales de España.</p>
-                    <p className={styles.ilia__text}>El 11 de octubre de 2020 hizo su debut en la UFC, peleando como reemplazo de <b>Seung Woo Choi</b> contra <b>Youssef Zalal</b>. Y pese al corto aviso que tuvo para pelear, logró ganar la pelea por decisión unánime.</p>
-                    <p className={styles.ilia__text}>Rápidamente después de su primera pelea, logró destacarse al ganar 2 combates por KO en el primer asalto contra <b>Damon Jackson</b> y <b>Ryan Hall</b>, y al ganar otra vez por KO en el segundo asalto contra <b>Jai Herbert</b>. Estás destacadas actuaciones lo llevaron a pelear en diciembre de 2022 contra <b>Bryce Mitchell</b>, a quien derrotó en el segundo asalto gracias a una esplendida sumisión, acabando así con la racha de 6 victorias consecutivas que había cosechado el peleador americano.</p>
-                    <Image className={styles.ilia__article__image} src={ilia} alt="Ilia topuria festejando una victoria de ufc despues de combatir contra Bryce Mitchel" />
-                    <p className={styles.ilia__text}>En su última pelea en junio de 2023, venció por decisión unánime a Josh Emmet, logrando de esta forma para muchos, ser merecedor de una pelea por el título contra <b>Alexander Volkanovski</b></p>
+            <section className={styles.peleador__articles__container}>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Ilia Topuria ufc</h2>
+                    <p className={styles.peleador__article__text}>Ilia Topuria es un luchador de nacionalidad georgiana y española de ufc que compite en la categoría de Peso Pluma. Pelea a nivel profesional como luchador de MMA desde 2015, cuando comenzó a competir en promotoras locales de España.</p>
+                    <p className={styles.peleador__article__text}>El 11 de octubre de 2020 hizo su debut en la UFC, peleando como reemplazo de <b>Seung Woo Choi</b> contra <b>Youssef Zalal</b>. Y pese al corto aviso que tuvo para pelear, logró ganar la pelea por decisión unánime.</p>
+                    <p className={styles.peleador__article__text}>Rápidamente después de su primera pelea, logró destacarse al ganar 2 combates por KO en el primer asalto contra <b>Damon Jackson</b> y <b>Ryan Hall</b>, y al ganar otra vez por KO en el segundo asalto contra <b>Jai Herbert</b>. Estás destacadas actuaciones lo llevaron a pelear en diciembre de 2022 contra <b>Bryce Mitchell</b>, a quien derrotó en el segundo asalto gracias a una esplendida sumisión, acabando así con la racha de 6 victorias consecutivas que había cosechado el peleador americano.</p>
+                    <Image className={styles.peleador__article__image} src={ilia} alt="Ilia topuria festejando una victoria de ufc despues de combatir contra Bryce Mitchel" />
+                    <p className={styles.peleador__article__text}>En su última pelea en junio de 2023, venció por decisión unánime a Josh Emmet, logrando de esta forma para muchos, ser merecedor de una pelea por el título contra <b>Alexander Volkanovski</b></p>
                 </article>
                 <article>
-                    <h2 className={styles.ilia__title}>Ilia Topuria ranking</h2>
-                    <p className={styles.ilia__text}>Actualmente se encuentra ranqueado 5to en la división de Peso Pluma. Ilia mantiene un record de 14 victorias sin ninguna derrota y ningún empate. De las cuales 4 fueron por la vía del nocaut, 8 por la sumisión y 2 por decisión.</p>
-                    <p className={styles.ilia__text}>Ilia venía de estar ranqueado en la 9na posición después de someter a Bryce Mitchell, y logró ganarse ese 5to puesto después de ganar por desición unánime contra Josh Emmet.</p>
+                    <h2 className={styles.peleador__article__title}>Ilia Topuria ranking</h2>
+                    <p className={styles.peleador__article__text}>Actualmente se encuentra ranqueado 5to en la división de Peso Pluma. Ilia mantiene un record de 14 victorias sin ninguna derrota y ningún empate. De las cuales 4 fueron por la vía del nocaut, 8 por la sumisión y 2 por decisión.</p>
+                    <p className={styles.peleador__article__text}>Ilia venía de estar ranqueado en la 9na posición después de someter a Bryce Mitchell, y logró ganarse ese 5to puesto después de ganar por desición unánime contra Josh Emmet.</p>
                 </article>
-                <article className={styles.ilia__article__container}>
-                    <h2 className={styles.ilia__title}>Última pelea de Topuria</h2>
-                    <p className={styles.ilia__text}>Su última pelea fue contra Josh Emmet, que en el momento de la pelea estaba ranqueado 5to en la división y que venía de una derrota contra Yair Rodriguez. Por su lado, Ilia venía de vencer a Bryce Mitchell por sumisión en el 2do round.</p>
-                    <p className={styles.ilia__text}>Había una gran expectativa sobre este combate, ya que Ilia Topuria tendría la oportunidad de mostrarse y ganarse una oportunidad por el título ante un top5 de la división, además de que por primera vez protagonizaría la cartelera estelar de UFC.</p>
-                    <iframe className={styles.ilia__video} width="560" height="315" src="https://www.youtube.com/embed/MswUiwOftB4?si=HujlFHYjgG8fR2kq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Última pelea de Topuria</h2>
+                    <p className={styles.peleador__article__text}>Su última pelea fue contra Josh Emmet, que en el momento de la pelea estaba ranqueado 5to en la división y que venía de una derrota contra Yair Rodriguez. Por su lado, Ilia venía de vencer a Bryce Mitchell por sumisión en el 2do round.</p>
+                    <p className={styles.peleador__article__text}>Había una gran expectativa sobre este combate, ya que Ilia Topuria tendría la oportunidad de mostrarse y ganarse una oportunidad por el título ante un top5 de la división, además de que por primera vez protagonizaría la cartelera estelar de UFC.</p>
+                    <iframe className={styles.peleador__article__video} width="560" height="315" src="https://www.youtube.com/embed/MswUiwOftB4?si=HujlFHYjgG8fR2kq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </article>
             </section>
         </main>
