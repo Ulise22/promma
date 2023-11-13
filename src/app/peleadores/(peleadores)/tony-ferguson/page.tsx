65a07fe10e5e74c3089cd3c3d12dcaec638c8648
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-13 12:23:21 -0300

    conor article done

--- src/app/peleadores/(peleadores)/tony-ferguson/page.tsx
+++ src/app/peleadores/(peleadores)/tony-ferguson/page.tsx
@@ -1,9 +1,11 @@
-import styles from './Ferguson.module.css'
+import PeleadoresHero from '../../components/PeleadoresHero'
+import ferguson from '../../../../assets/peleadores__images/tony-ferguson/ferguson.png'
+import styles from '../../components/peleador.module.css'
 
 export default function Ferguson () {
     return(
         <main>
-            
+            <PeleadoresHero peleadoresImage={ferguson} w={26} l={9} d={0} nombre='Tony Ferguson' categoria='Peso Ligero' apodo='EL CUCUY' />
             <section>
                 <h1>Tony Ferguson</h1>
             </section>
