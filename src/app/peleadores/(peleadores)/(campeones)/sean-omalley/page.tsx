62b294025a765db6ec2ad7170a3712dc89d7c3da
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-14 17:36:51 -0300

    sean o'malley & tony ferguson articles done

--- src/app/peleadores/(peleadores)/(campeones)/sean-omalley/page.tsx
+++ src/app/peleadores/(peleadores)/(campeones)/sean-omalley/page.tsx
@@ -1,6 +1,10 @@
 import PeleadoresHero from '../../../components/PeleadoresHero'
 import omalley from '../../../../../assets/peleadores__images/sean-omalley/omalley.png'
+import pelea from '@/assets/peleadores__images/sean-omalley/omalley_pelea.jpg'
+import careo from '@/assets/peleadores__images/sean-omalley/omalley_careo.jpg'
+import sean from '@/assets/peleadores__images/sean-omalley/sean_omalley.jpg'
 import styles from '../../../components/peleador.module.css'
+import Image from 'next/image'
 
 export default function Omalley () {
     return(
@@ -9,17 +13,28 @@ export default function Omalley () {
             <section className={styles.peleador__articles__container}>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Sean O&apos;malley UFC</h2>
+                    <p className={styles.peleador__article__text}>Comenzó su trayecto de pelea en su ciudad natal, montana, luego se mudó a Dakota del norte y posteriormente luchó para Legacy Fighting Alliance, donde obtuvo un nocaut contra David Nuzzo. Tiempo más tarde, tras derribar a Alfred Khashakyan, recibió un contrato de UFC. El 1 de diciembre de 2017 tuvo su debut oficial contra Terrion Ware en The Ultimate Fighter 26 Finale donde ganó por decisión unánime. Llevando una trayectoria de 6 años, campeón de UFC 292 peso gallo (19/08/2023), ganando por KO en el segundo round.</p>
+                    <p className={styles.peleador__article__text}>Con un total de 11 peleas oficiales en la UFC (15/08/2020) y 1 derrota contra Marlon Vera (15/08/202).</p>
+                    <Image className={styles.peleador__article__image} src={careo} alt='' />
+                    <p className={styles.peleador__article__text}>O&apos;Malley Habló sobre el salario, diciendo: “Es por eso que argumento que me deberían pagar jodidamente más. Porque amigo, quiero decir de manera realista, con quien sea que pelee a continuación, no tengo que vender esa pelea, porque no me pagarán más si son cinco o un millón de personas las que miran la pelea. Debería haber incentivos allí. Lo haces si estás en el evento principal y obtienes puntos de PPV. Pero incluso en mi posición, debería poder obtener puntos de PPV ya que puedo vender la pelea.”</p>
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Sean O&apos;malley Record</h2>
+                    <p className={styles.peleador__article__text}>Total de 19 peleas, 17 victorias, 12 continúas, 12 por nocaut, 1 por sumisión y 4 por decisión. 11 peleas oficiales en la UFC, 9 victorias, 6 por nocaut y 3 por decisión. 1 sin resultado y 1 derrota por nocaut. </p>
+                    <Image className={styles.peleador__article__image} src={sean} alt='' />
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Sean O&apos;malley Historia</h2>
+                    <p className={styles.peleador__article__text}>Nacido un 24 de octubre de 1994, Helena, Montana. Fue criado por su padre, oficial de narcóticos retirado. No se sabe mucho sobre su madre. Actualmente en relación con Danya Gonzalez y una hija, Elena. El luchador tiene un podcast propio llamado “Timbo Sugar Show” donde habla sobre las artes marciales, conversando con especialistas y amigos. </p>
+                    <p className={styles.peleador__article__text}>“Uso al cannabis para relajarme por la noche. Es como si mi cuerpo lo supiera bien: nos estamos relajando.” Afirma Sean O&apos;Malley. Suele publicar fotos y vídeos fumando marihuana con variados tipos de pipas, con porros, en su hogar o fuera de este. “Uso el cannabis para relajarme por las noches. A eso le sumo un chapuzón en el jacuzzi, lo hago religiosamente.”</p>
+                    <Image className={styles.peleador__article__image} src={pelea} alt='' />
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Sean O&apos;malley Última Pelea</h2>
+                    <p className={styles.peleador__article__text}>Su última pelea fue contra Aljamain Sterling por el título UFC 292, en la cual venció por nocaut en el segundo round, minuto 0:51. Tras su victoria, comentó: “Siempre digo que conmigo sólo hace falta un error (para vencerte). Ni siquiera sé si eso es un error. Soy así de bueno.”</p>
+                    <p className={styles.peleador__article__text}>Pidió la revancha contra Marlon Vera, comentando: “Éste es sólo el principio de la era ‘Suga’. Llevaré esta mier** hasta 2035. Le patearé el c*** a ‘Chito’ en diciembre en Las Vegas en el T-Mobile.” </p>
+                    <iframe className={styles.peleador__article__video} width="560" height="315" src="https://www.youtube.com/embed/m0C_6mzXyxY?si=vuDCdqGJnpadpB-_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </article>
-                
             </section>
         </main>
     )
--- src/app/peleadores/(peleadores)/tony-ferguson/page.tsx
+++ src/app/peleadores/(peleadores)/tony-ferguson/page.tsx
@@ -1,13 +1,43 @@
 import PeleadoresHero from '../../components/PeleadoresHero'
 import ferguson from '../../../../assets/peleadores__images/tony-ferguson/ferguson.png'
+import pelea from '@/assets/peleadores__images/tony-ferguson/ferguson_pelea.webp'
+import cinturon from '@/assets/peleadores__images/tony-ferguson/ferguson_cinturon.webp'
+import win from '@/assets/peleadores__images/tony-ferguson/ferguson_ganando.webp'
 import styles from '../../components/peleador.module.css'
+import Image from 'next/image'
 
 export default function Ferguson () {
     return(
         <main>
             <PeleadoresHero peleadoresImage={ferguson} w={26} l={9} d={0} nombre='Tony Ferguson' categoria='Peso Ligero' apodo='EL CUCUY' />
-            <section>
-                <h1>Tony Ferguson</h1>
+            <section className={styles.peleador__articles__container}>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Ferguson UFC</h2>
+                    <p className={styles.peleador__article__text}>Anthony Ferguson ha peleado en UFC durante poco más de 10 años. Tiempo en el que consiguió ser campeón de The Ultimate Fighter 13 y UFC 216. Firmó con la UFC en 2011 para competir en The Ultimate Fighter de parte del equipo Lesnar. Actualmente se encuentra en el equipo Snap Down City Academy.</p>
+                    <Image className={styles.peleador__article__image} src={pelea} alt='El peleador de mma Tony Ferguson golpeando en el rostro a otro peleador de mma en un combate de UFC' />
+                    <p className={styles.peleador__article__text}>Ferguson, hizo oficial su debut en el UFC en The Ultimate Fighter 13 contra Ramsey Nijem (04/06/2011) para definir al ganador de The Ultimate Fighter 13. Ferguson ganó por KO en la primera ronda, así como también el KO de la noche por su victoria contra Nijem. </p>
+                    <p className={styles.peleador__article__text}>(07/11/2017) Ferguson se enfrentó contra Kevin Lee en UFC 216 por el Campeonato Interino de Peso Ligero de UFC donde ganó por sumisión en el tercer asalto. No dudó, luego de su victoria, retar a McGregor (ganador del cinturón absoluto) sin embargo, no hubo más que especulaciones al respecto, puesto que McGregor prefirió enfrentarse a Khabib Nurmagomedov.</p>
+                    <p className={styles.peleador__article__text}>Tony Ferguson habló sobre su disgusto en los salarios en UFC: “Creo que estamos siendo mal pagados. Mi valor está aquí y creo que deberían pagarme por eso. También pienso que deberíamos tener un seguro. Pienso muchas cosas… somos malditos atletas, de los mejores del mundo. Cuando leí a Dana decir el otro día que a los boxeadores se les paga en exceso yo dije: ¡Claro que no!” (04/05/2022)</p>
+                </article>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Tony Ferguson Biografía</h2>
+                    <p className={styles.peleador__article__text}>Nacido un 12 de febrero de 1984, midiendo 1,80 y pesando 70kg (154 lb.), peleador estadounidense de artes marciales mixtas. Actualmente compite en la categoría de peso ligero de UFC desde 2011. Ganador de The Ultimate Fighter 13 contra Ramsey Nijem y del título Interino de Peso Ligero de UFC 216 contra Kevin Lee.</p>
+                    <Image className={styles.peleador__article__image} src={cinturon} alt='El artista marcial Tony Ferguson sosteniendo su cinturon de Peso Ligero de UFC en una conferencia de prensa' />
+                    <p className={styles.peleador__article__text}>Ferguson se crio en Muskegon, Míchigan. Una pequeña ciudad del medio oeste, donde su padre decide inscribirlo en los deportes a una edad temprana para que hiciese amistades y pudiese encajar en su nuevo entorno. Ganador de 12 cartas de equipo universitario, defensor de los campeones estatales de 2002 y 3 veces All-State selection en Wrestling, victorioso de la división de 152 lb. en 2002.  Ganó el All-American honors dos veces, ganando el campeonato nacional en la división de 165 lb.</p>
+                    <p className={styles.peleador__article__text}>Trabajó en marketing y ventas, así como también de camarero. Una vez que su jefe del bar invitó a Ferguson a trabajar con jóvenes de MMA, se enamoró del deporte y desde entonces no ha dejado de competir.</p>
+                </article>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Tony Ferguson Récord</h2>
+                    <p className={styles.peleador__article__text}>Tiene un récord de 15 victorias, 12 victorias continúas, 5 nocaut, 6 sumisión y 4 por decisión. 7 derrotas, 2 nocaut, 2 sumisión y 3 por decisión. (UFC)</p>
+                    <Image className={styles.peleador__article__image} src={win} alt='Tony Ferguson festejando luego de dejar KO a su rival en un combate de ufc' />
+                </article>
+                <article className={styles.peleador__article}>
+                    <h2 className={styles.peleador__article__title}>Tony Ferguson Última Pelea</h2>
+                    <p className={styles.peleador__article__text}>Tony Ferguson se enfrentó a Bobby Green y perdió por sumisión. El primer asalto se vio afectado tras un piquete de ojos a Ferguson, obligando a los médicos ingresar al octágono. Momentos después, la pelea continuó.</p>
+                    <p className={styles.peleador__article__text}>En el segundo round, Green mantuvo a ralla a Ferguson un par de ocasiones, sin embargo, no fueron suficientes para ganarle, generando un último minuto con buenos intercambios de golpes donde Green parecía mejor parado.</p>
+                    <p className={styles.peleador__article__text}>Durante un total de tres rounds, Green consiguió someter a Ferguson tras un descuido, a tan solo seis segundos de terminar la pelea.</p>
+                    <iframe className={styles.peleador__article__video} width="560" height="315" src="https://www.youtube.com/embed/aYZgNn1aaaY?si=nmHsHT2Nk0EKIqlA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </article>
             </section>
         </main>
     )
