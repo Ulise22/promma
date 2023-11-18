ab6d947eb464090f0bb759ffed10b9fe8be1aee3
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-18 14:50:22 -0300

    Islam Makhachev article done

--- src/app/peleadores/(peleadores)/(campeones)/islam-makhachev/page.tsx
+++ src/app/peleadores/(peleadores)/(campeones)/islam-makhachev/page.tsx
@@ -1,6 +1,11 @@
 import PeleadoresHero from '../../../components/PeleadoresHero'
-import makhachev from '../../../../../assets/peleadores__images/islam-makhachev/makhachev.png'
+import makhachev from '@/assets/peleadores__images/islam-makhachev/makhachev.png'
+import khabib from '@/assets/peleadores__images/islam-makhachev/makhachev_khabib.jpg'
+import islam from '@/assets/peleadores__images/islam-makhachev/islam__makhachev.jpg'
+import pelea from '@/assets/peleadores__images/islam-makhachev/makhachev_pelea.jpg'
 import styles from '../../../components/peleador.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Makhachev () {
     return(
@@ -9,21 +14,34 @@ export default function Makhachev () {
             <section className={styles.peleador__articles__container}>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>UFC Makhachev</h2>
+                    <p className={styles.peleador__article__text}>Makhachev es uno de los principales puntuales de la UFC. Uno de los peleadores más completos de la empresa. Desde el 7 de noviembre está en la primera posición del ranking libra por libra de UFC.</p>
+                    <p className={styles.peleador__article__text}>En 2014 firmó su contrato de 4 peleas en la UFC. Tuvo su debut oficial el 23 de mayo de 2015, consiguiendo su primera victoria contra Leo Kuntz, donde ganó por sumisión. Actualmente lleva más de 5 años dentro de la UFC, donde ha conseguido ganar (en el año 2022) su primer campeonato de peso ligero de UFC contra <Link href='/peleadores/charles-oliveira'>Charles Oliveira</Link> (UFC 280) y ha podido defenderlo en dos ocasiones contra Alexander Volkanovski (UFC 284 y UFC 294)</p>
+                    <Image className={styles.peleador__article__image} src={islam} alt='Islam Makhachev proclamándose campeón de Peso Ligero de UFC luego de derrotar a su rival Charles Oliveira' />
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Makhachev MMA</h2>
+                    <p className={styles.peleador__article__text}>Hizo su debut en M-1 Global contra Tengiz Kuchua en 2011, saliendo victorioso en el primer round por nocaut. En 2014 hizo su última pelea contra Ivica Truscek, ganando en el tercer round por sumisión. Luego de esto firmó su contrato para UFC.</p>
+                    <p className={styles.peleador__article__text}>No ha perdido ninguna pelea en M-1, teniendo un total de 5 enfrentamientos victoriosos.</p>
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Makhachev Record</h2>
+                    <p className={styles.peleador__article__text}>25-1-0: Lleva un recorrido total de 26 peleas: 1 sola derrota por nocaut contra Adriano Martins. 25 victorias, 11 por sumisión, 5 por nocaut y 9 por decisión unánime y decisión dividida. Tiene 15 peleas oficiales de UFC, de las cuales solo ha perdido 1 por nocaut y ha ganado 14: 6 por sumisión, 3 por nocaut y 5 por decisión unánime. </p>
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Islam Makhachev & Khabib Nurmagomédov</h2>
+                    <Image className={styles.peleador__article__image} src={khabib} alt='Islam Makhachev junto a Khabib Numagomédov y el padre de Khabib'/>
+                    <p className={styles.peleador__article__text}>Nacido el 27 de octubre de 1991, Majachkala, Duguestán (Rusia). A los 11 años de edad, en la escuela, conoce a Abubakar, sobrino de Abdulmanap Nurmagomedov, exmilitar y exjudoca ruso. Abubakar entrenaba con Khabib, hijo de Abdulmanap, de esta forma comienza la amistad entre Khabib e Islam (menor por dos años)</p>
+                    <p className={styles.peleador__article__text}>De esta manera, Makhachev, se volvió parte de la familia Nurmagomedov. En reiterarás ocasiones, ambos han declarado que se sienten como hermanos. Abdumanap se encargó de entrenar a ambos peleadores. Y aunque actualmente Khabib no está en el equipo de Makhachev, su relación sigue siendo igual.</p>
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Makhachev Última pelea</h2>
+                    <p className={styles.peleador__article__text}>Su última pelea fue una revancha contra <Link href='/peleadores/alexandder-volkanovski'>Alexander Volkanovski</Link> donde consiguió defender su título, ganando por nocaut en el primer round. Se tenía previsto que peleará contra Charles Oliveira, pero Oliveira fue obligado a retirarse del evento por lesión.</p>
+                    <Image className={styles.peleador__article__image} src={pelea} alt='Islam Makhachev golpeando en el rsotro a su rival ded UFC Alexander Volkanovski' />
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Makhachev Próxima Pelea</h2>
+                    <p className={styles.peleador__article__text}>El entrenador Javier Méndez comenta: “Porque vencer a Conor no creará el tipo de legado que el Islam quiere, ¿verdad? Ir tras el título de peso welter sí lo es. Ese es un momento de tipo legado.” Esto genera que Edwards o Covington sean potenciales rivales a enfrentar en peso welter el próximo 16 de diciembre.</p>
+                    <p className={styles.peleador__article__text}>Sin embargo, esto parece poco probable. Uno de los nombres más sonados sigue siendo el de Charles Oliveira por la revancha. </p>
                 </article>
             </section>
         </main>
--- src/app/peleadores/(peleadores)/michael-chandler/page.tsx
+++ src/app/peleadores/(peleadores)/michael-chandler/page.tsx
@@ -1,7 +1,11 @@
 import PeleadoresHero from '../../components/PeleadoresHero'
 import chandler from '@/assets/peleadores__images/michael-chandler/chandler.png'
+import trump from '@/assets/peleadores__images/michael-chandler/chandler_trump.jpg'
+import conor from '@/assets/peleadores__images/michael-chandler/chandler-vs-mcgregor.jpg'
+import michael from '@/assets/peleadores__images/michael-chandler/michael_chandler.jpg'
 import styles from '../../components/peleador.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Chandler () {
     return(
@@ -11,6 +15,7 @@ export default function Chandler () {
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Chandler UFC</h2>
                     <p>Michael Chandler es actualmente el número #5 de la división de Peso Ligero de <Link href='/promotoras/ufc'>UFC</Link>, debido a la extensa carrera que llevaba Chandler en <b>Bellator</b>, siendo campeón de su división, al momento de entrar en la UFC, apareció muy alto en los rankings.</p>
+                    <Image className={styles.peleador__article__image} src={michael} alt='El peleador de MMA Michael Chandler en el reality de televisión de UFC, The Ultimate Fighter' />
                     <p>Dado al prestigio que cosecho en su carrera antes de llegar a UFC, la primera pelea que tuvo fue contra un top10 como lo es <b>Dan Hooker</b>, a quien derrotó por nocaut a los 2 minutos del primer asalto. Victoria, que en su segundo combate apenas, lo llevó a pelear por el Campeonato de Peso Ligero que había quedado vacante luego de la retirada de <b>Khabib</b>, contra el brasileño <b>Charles Oliveira</b>. Finalmente la pelea terminó con una victoria para el brasileño en el segundo asalto por TKO.</p>
                 </article>
                 <article className={styles.peleador__article}>
@@ -20,10 +25,12 @@ export default function Chandler () {
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Michael Chandler Historia</h2>
                     <p>Michael Chandler Jr. nació el 24 de abril de 1986 en Nashville, Estados Unidos. Es el segundo de 4 hermanos e hijo de Michael Chandler Sr. y de Betty Chandler. Durante su adolescencia peleó como luchador y terminó segundo en el Campeonato de escuelas secundarias del Estado de Misuri.</p>
+                    <Image className={styles.peleador__article__image} src={trump} alt='El peleador de UFC Michael Chandler junto a su esposa y junto a el presidente de los Estados Unidos Donald Trump' />
                     <p>Luego de graduarse en 2004, Chandler entró a la Universidad de Misuri, donde se unió al equipo de wrestling. Chandler clasificó cuatro veces a la NCAA, consiguió 100 victorias en su carrera.</p>
                     <p>Una vez terminada su carrera de wrestling, Michael Chandler comenzó a entrenar MMA donde comenzaría a competir de forma profesional en agosto de 2009, donde debutaría con una victoria por TKO en el primer asalto.</p>
                     <p>En septiembre de 2010 debutaría en Bellator con un nocaut en el primer asalto. En esta compañía es donde pasó la mayor parte de su carrera, compitiendo en Peso Ligero y siendo campeón de esa división 3 veces, teniendo varias defensas por el título exitosas entre medio.</p>
                     <p>Actualmente se encuentra peleando en UFC por la división de Peso Ligero. Donde se encuentra rankeado #5 y en donde ya ha tenido la oportunidad de pelear por el título de la división.</p>
+                    <Image className={styles.peleador__article__image} src={conor} alt='Michael Chandler y Conor Mcgregor viendose cara a cara' />
                 </article>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Michael Chandler Última Pelea</h2>
