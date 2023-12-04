6804c22477c49eb9d4d4e2c7d21f6b0614fbf174
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-12-04 17:25:56 -0300

    replace navbar for a menu dropdown

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from 'react'
+import styles from './Menu.module.css'
+import Image from 'next/image'
+import logo from '../../assets/PROMMA.png'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+export default function Menu () {
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+        <nav className={styles.menu}>
+            <Link className={styles.menu__logo} href='/'>
+                <Image className={styles.menu__logo__img} src={logo} alt='ProMMA' />
+            </Link>
+
+            <div onClick={() => setIsOpen(!isOpen)} className={styles.menu__icon}>
+                { isOpen ? <FontAwesomeIcon icon={faXmark} size='xl' /> : <FontAwesomeIcon icon={faBars} size='xl' /> } 
+            </div>
+            
+            <ul className={styles.menu__list}>
+                <li className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/'>Inicio</Link></li>
+                <li className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/peleadores'>Peleadores</Link>
+                    <ul className={styles.menu__list__dropdown}>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/ilia-topuria'>Ilia Topuria</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/conor-mcgregor'>Conor Mcgregor</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/francis-ngannou'>Francis Ngannou</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/tony-ferguson'>Tony Ferguson</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/dustin-poirier'>Dustin Poirier</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/peleadores/justin-gaethje'>Justin Gaethje</Link></li>
+                    </ul>
+                </li>
+                <li className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/estilos'>Estilos</Link>
+                    <ul className={styles.menu__list__dropdown}>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/estilos/boxeo'>Boxeo</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/estilos/jiujitsu'>Jiu Jitsu</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/estilos/muay-thai'>Muay Thai</Link></li>
+                    </ul>
+                </li>
+                <li className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/promotoras'>Promotoras</Link>
+                    <ul className={styles.menu__list__dropdown}>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/promotoras/ufc'>UFC</Link></li>
+                        <li><Link className={styles.menu__list__dropdown__link} href='/promotoras/pfl'>PFL</Link></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Menu from './components/Menu'
 config.autoAddCss = false
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,7 +26,7 @@ export default function RootLayout({
         <meta name="google-adsense-account" content="ca-pub-9270621638584149"></meta>
       </head>
       <body className={inter.className}>
-        <Navbar />
+        <Menu />
         {children}
         <Footer />
       </body>
