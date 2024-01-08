d63d6a4d6f00d3cc473166e8425f07338f8fce45
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-08 11:25:30 -0300

    add classNames to the DWT article

--- src/app/promotoras/king-of-the-streets/page.tsx
+++ src/app/promotoras/king-of-the-streets/page.tsx
@@ -7,7 +7,8 @@ export default function KOTS () {
             <PromotorasHero title='King Of The Streets' />
             <section className={styles.promotoras__container}>
                 <article className={styles.promotoras__article}>
-                    <h2>KOTS MMA</h2>
+                    <h2 className={styles.promotoras__article__title}>KOTS MMA</h2>
+                    <p>King Of The Streets es una organización clandestina que prepara peleas callejeras sin reglas donde vale todo, apenas un vendaje cubre las manos de los peleadores, y ambos se golpean sin parar hasta que uno se rinde. </p>
                 </article>
             </section>
         </main>
