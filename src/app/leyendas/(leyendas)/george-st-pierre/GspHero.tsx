a035239673c2e6f0578e11187ca07fea349ea911
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-16 13:03:19 -0300

    gsp article done

--- src/app/leyendas/(leyendas)/george-st-pierre/GspHero.tsx
+++ src/app/leyendas/(leyendas)/george-st-pierre/GspHero.tsx
@@ -2,8 +2,13 @@ import styles from './gspHero.module.css'
 
 export default function GspHero () {
     return(
-        <section className={styles.leyenda__hero}>
-            <h1 className={styles.leyenda__hero__title}>George St-Pierre</h1>
-        </section>
+        <>
+            <section className={styles.leyenda__hero}>
+                <h1 className={styles.leyenda__hero__title}>George St-Pierre</h1>
+            </section>
+            <section className={styles.leyenda__record__container}>
+                <b className={styles.leyenda__record}>26-2</b>
+            </section>
+        </>
     )
 }
