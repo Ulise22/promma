3f1df206de6c9001138a7270b9cafeb2f03576a9
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-15 21:28:12 -0300

    khabib article almost done

--- src/app/leyendas/(leyendas)/khabib-nurmagomedov/KhabibHero.tsx
+++ src/app/leyendas/(leyendas)/khabib-nurmagomedov/KhabibHero.tsx
@@ -2,8 +2,13 @@ import styles from './khabibHero.module.css'
 
 export default function KhabibHero () {
     return(
-        <section className={styles.leyenda__hero}>
-            <h1 className={styles.leyenda__hero__title}>Khabib Nurmagomedov</h1>
-        </section>
+        <>
+            <section className={styles.leyenda__hero}>
+                <h1 className={styles.leyenda__hero__title}>Khabib Nurmagomedov</h1>
+            </section>
+            <section className={styles.leyenda__record__container}>
+                <b className={styles.leyenda__record}>29-0</b>
+            </section>
+        </>
     )
 }
