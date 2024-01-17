a1840071ac7da0fda2a1810f93c8e2adf53dbc2d
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-17 14:30:05 -0300

    brock lesnar article done

--- src/app/leyendas/(leyendas)/brock-lesnar/LesnarHero.tsx
+++ src/app/leyendas/(leyendas)/brock-lesnar/LesnarHero.tsx
@@ -2,8 +2,13 @@ import styles from './lesnarHero.module.css'
 
 export default function LesnarHero () {
     return(
-        <section className={styles.leyenda__hero}>
-            <h1 className={styles.leyenda__hero__title}>Brock Lesnar</h1>
-        </section>
+        <>
+            <section className={styles.leyenda__hero}>
+                <h1 className={styles.leyenda__hero__title}>Brock Lesnar</h1>
+            </section>
+            <section className={styles.leyenda__record__container}>
+                <b className={styles.leyenda__record}>5-3-(1)</b>
+            </section>
+        </>
     )
 }
--- src/app/leyendas/(leyendas)/daniel-cormier/CormierHero.tsx
+++ src/app/leyendas/(leyendas)/daniel-cormier/CormierHero.tsx
@@ -2,8 +2,13 @@ import styles from './cormierHero.module.css'
 
 export default function CormierHero () {
     return(
-        <section className={styles.leyenda__hero}>
-            <h1 className={styles.leyenda__hero__title}>Daniel Cormier</h1>
-        </section>
+        <>
+            <section className={styles.leyenda__hero}>
+                <h1 className={styles.leyenda__hero__title}>Daniel Cormier</h1>
+            </section>
+            <section className={styles.leyenda__record__container}>
+                <b className={styles.leyenda__record}>22-3-(1)</b>
+            </section>
+        </>
     )
 }
