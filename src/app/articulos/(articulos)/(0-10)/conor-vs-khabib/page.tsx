f7600ecc5038a44d9cac156e208d487ec8395de1
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-21 22:18:08 -0300

    ufc300 article done

--- src/app/articulos/(articulos)/(0-10)/conor-vs-khabib/page.tsx
+++ src/app/articulos/(articulos)/(0-10)/conor-vs-khabib/page.tsx
@@ -3,15 +3,23 @@ import ArticleHero from '@/app/articulos/components/ArticleHero'
 import AsideChamps from '@/app/components/asides/AsideChamps'
 /* Images */
 import hero from '@/assets/peleadores__images/peleadores__hero.jpg'
+import Link from 'next/link'
 
 export default function KhabibVsConor () {
     return(
         <main>
-            <ArticleHero title='Conor McGregor vs Khabib Nurmagomedov: ¿Qué pasó realmente?' subtitle='¿Cuál es la historia de la rivalidad más grande en la historia de las MMA? Desde el ataque de Conor a Khabib en el bus, hasta la vez que Khabib se fue a pelear con la esquina de Conor...' image={hero} />
+            <ArticleHero title='Conor McGregor vs Khabib Nurmagomedov: ¿Qué pasó Realmente Antes y Después de la Pelea?' subtitle='Vamos a hablar de la razón de tanta rivalidad entre estos 2 peleadores, por qué Khabib luego del combate fue a pelear con alguien de la esquina de Conor, el incidente del bus...' image={hero} />
             <section className={styles.article__container}>
                 <article className={styles.article}>
-                    <h2>xd</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim dolores culpa ad doloribus amet. Sequi consequatur quae officiis, quod exercitationem rem tempore fugit deleniti earum explicabo tempora dolor? Labore.</p>
+                    <h2>Dillon Danis y Khabib</h2>
+                    <p>Aparentemente el &quot;beef&quot; que había entre McGregor y Khabib, comenzaría por culpa de Dillon Danis, quien es un amigo cercano y compañero de entrenamiento de Conor, que es además especialista en Jiu-Jitsu. Este se habría ofendido y le habría molestado ver la imagen de Khabib en la que llevaba puesta una remera que decía &quot;Si el Sambo fuera fácil se llamaría Jiu-Jitsu&quot;, luego de esto Danis afirmaría que él podría vencerlo facilmente en un duelo de grappling, además de afirmar cosas como que Conor podría enfrentarse en suelo al ruso, y además sería capaz de someterlo. Sin embargo, en un principio Khabib no le hizo mucho caso.</p>
+                    <h2>El incidente del Bus de McGregor</h2>
+                    <p>Este conflicto al parecer se daría luego de que <Link href='/leyendas/khabib-nurmagomedov'>Khabib Nurmagomedov</Link> y <b>Artem Lobov</b> tuvieran un pequeño altercado, donde el ruso y su equipo acorralarían a Lobov, intimidandolo. Este, le contaría lo sucedido a <Link href='/peleadores/conor-mcgregor'>Conor McGregor</Link>, que es un amigo cercano de él, y en respuesta decidirían, junto a otras 20 personas con credenciales de su equipo, ingresar al Braclays Center el 5 de abril durante la audiencia promocional para UFC 223, con el objetivo de increpar a Khabib, que estaba abandonando la arena junto a otros peleadores que también iban a participar del evento. Mientras el autobus se iba retirando lentamente, McGregor lanzó una carretilla de carga en forma de L a este, rompiendo las ventanillas del autobús, dejando en el camino a los peleadores Ray Borg y Michael Chiesa heridos en el camino por los cristales, luego de ir al hospital se decidió que por su salud no participarían del UFC 223 como estaba planeado en un principio.</p>
+                    <iframe className={styles.article__video} width="560" height="315" src="https://www.youtube.com/embed/Xc56YrGbwu0?si=5pM7c1wQS83cJ9ki" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    <p>Como resultado de estas acciones Conor recibió una orden de arresto, fue acusado de con 3 cargos de agresión y un cargo de delito criminal. Sin embargo, pese a que aquella noche se entregó se declaro no culpable, siendo finalmente declarado culpable de cargo de conducta desordenada y se le ordenó realizar cinco días de servicio comunitario y asistir a clases de manejo de la ira.</p>
+                    <p>Luego de todo este incidente, se terminaría anunciando una pelea entre el irlandés y el ruso para el 6 de octubre de 2018, en Paradise, Nevada por UFC 229.</p>
+                    <h2>El incidente después de la pelea</h2>
+                    <p>Al combate ya se había llegado con un combate bastante caldeado, más allá del incidente del autobus y de las palabras de Dillon Danis. En la previa Conor se había dedicado a hablar mierda de todo lo que estuviera relacionado a Khabib buscando así meterse en su cabeza y sacar alguna ventaja deportiva, además de vender la pelea. En el proceso se metió con el mismo Khabib, con su familia, con su esposa, con su religión y hasta con el presidente de su país.</p>
                 </article>
                 <AsideChamps />
             </section>
--- src/app/articulos/(articulos)/(0-10)/ufc300/page.tsx
+++ src/app/articulos/(articulos)/(0-10)/ufc300/page.tsx
@@ -3,6 +3,7 @@ import ArticleHero from '@/app/articulos/components/ArticleHero'
 import AsideChamps from '@/app/components/asides/AsideChamps'
 /* Images */
 import hero from '@/assets/articulos/previews/ufc300.jpg'
+import Link from 'next/link'
 
 export default function UFC300 () {
     return(
@@ -10,8 +11,16 @@ export default function UFC300 () {
             <ArticleHero title='UFC 300: Todas las Peleas Confirmadas Hasta Ahora' subtitle='Dana White ha prometido que este será el mayor evento de MMA en la historia, estos son los combates que confirmados hasta ahora!' image={hero} />
             <section className={styles.article__container}>
                 <article className={styles.article}>
-                    <h2>xd</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim dolores culpa ad doloribus amet. Sequi consequatur quae officiis, quod exercitationem rem tempore fugit deleniti earum explicabo tempora dolor? Labore.</p>
+                    <p>Dana White ha prometido mucho para este evento que apunta a ser histórico. Más allá de los rumores que hay para el día 13 de abril de 2024 que es para cuando está apuntado el UFC 300, que van desde el regreso de viejas glorias como <Link href='/leyendas/brock-lesnar'>Brock Lesnar</Link> o la vuelta de <Link href='/leyendas/khabib-nurmagomedov'>Khabib</Link>, estos son los combates confirmados hasta ahora: </p>
+                    <h2>Justin Gaethje vs Max Holloway por el BMF</h2>
+                    <p>Hasta ahora la mayor sorpresa y la pelea que más promete es esta, donde pondrán en juego el título del BMF (Baddest MotherFucker), luego de que <Link href='/peleadores/justin-gaethje'>Gaethje</Link> lo ganara en su pelea contra <Link href='/peleadores/dustin-poirier'>Dustin Poirier</Link> con una patada que dejó KO a Poirier. </p>
+                    <p>A este combate llega Gaethje, por su parte, como ya mencionamos, luego de vencer por nocaut a Poirier. Mientras que Max Holloway llega luego de vencer por nocaut a <b>The Korean Zombie</b>, en una división que, sacando a Volkanovski, Holloway lleva dominando desde hace rato.</p>
+                    <h2>Weili Zhang vs Yan Xiaonan</h2>
+                    <p>Otro de los combates interesantes que ya están confirmados, es la pelea por el tíulo del Peso Paja Femenino de UFC entre la vigente campeona, <Link href='/campeones/weili-zhang'>Weili Zhang</Link>, que viene de ganar su última pelea defendiendo el título contra la brasileña <b>Amanda Lemos</b> por decisión unánime, y viene con una racha de 3 victorias consecutivas, contra la también peleadora china <b>Yan Xiaonan</b>, que actualmente está posicionada #2 en la división y que en su último combate venció por KO a la brasileña <b>Jéssica Andrade</b> en el primer asalto, ganandose el premio a la actuación de la noche.</p>
+                    <h2>Jiří Procházka vs Aleksandar Rakić</h2>
+                    <p>El combate el número #2 de la división de Semipesados, Jiří Procházka, que viene de una derrota por nocat en su pelea por el título contra <Link href='/campeones/alex-pereira'>Alex Pereira</Link>, y el número #5 de la división, <b>Aleksandar Rakic</b> que viene de una derrota por TKO en el tercer asalto contra el polaco <b>Jan Błachowicz</b> hace casi 2 años.</p>
+                    <h2>Calvin Kattar vs Aljamain Sterling</h2>
+                    <p>Se confirmó la pelea entre el ex-campeón de Peso Gallo <b>Aljamain Sterling</b>, que viene de perder su título por nocaut en el segundo asalto contra el estadounidense <Link href='/campeones/sean-omalley'>Sean O&apos;Malley</Link>, contra el número #7 de la división de Peso Pluma <b>Calvin Kattar</b>, que decidió bajar de categoría para enfrentarse a Sterling luego de su derrota en octubre de 2022 contra el inglés <b>Arnold Allen</b> por TKO en el segundo asalto de la pelea.</p>
                 </article>
                 <AsideChamps />
             </section>
