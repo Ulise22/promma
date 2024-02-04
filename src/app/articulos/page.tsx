450464cd1772bdd9051b83d36704b1b235a7cacb
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-02-04 17:50:30 -0300

    glover-teixeira article done

--- src/app/articulos/page.tsx
+++ src/app/articulos/page.tsx
@@ -6,6 +6,7 @@ import conorVkhabib from '@/assets/peleadores__images/peleadores__hero.jpg'
 import mostppv from '@/assets/articulos/previews/mostppv.jpg'
 import khabibVferguson from '@/assets/articulos/previews/khabibVferguson.jpg'
 import cortePeso from '@/assets/articulos/previews/corte-peso.jpg'
+import texeira from '@/assets/articulos/previews/glover_teixeira.webp'
 
 export default function Articulos () {
     return(
@@ -16,6 +17,7 @@ export default function Articulos () {
                 <ArticlePreview img={mostppv} url='/articulos/most-ppv' title='Top 5: Las peleas de MMA que más PPV vendieron' />
                 <ArticlePreview img={khabibVferguson} url='/articulos/khabib-ferguson' title='Khabib vs Tony Ferguson: La pelea maldita que nunca pudo darse' />
                 <ArticlePreview img={cortePeso} url='/articulos/cortes-de-peso' title='Los durísimos cortes de peso a los que se someten los peleadores de UFC' />
+                <ArticlePreview img={texeira} url='/articulos/campeon-viejo-ufc' title='El campeón más viejo de la historia moderna de la UFC' />
             </section>
         </main>
     )
