dada89b4be9ed29a08abfe87fb845041898eb536
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-02-18 18:25:23 -0300

    ufc298 article done

--- src/app/articulos/components/PageHero.tsx
+++ src/app/articulos/components/PageHero.tsx
@@ -2,17 +2,16 @@ import Link from 'next/link'
 import styles from './PageHero.module.css'
 import Image from 'next/image'
 /* IMAGES */
-import ufc297 from '@/assets/articulos/previews/ufc297.jpg'
+import ufc298 from '@/assets/articulos/previews/ufc298_preview.jpg'
 import ufc300 from '@/assets/articulos/previews/ufc300.jpg'
 import dwt2 from '@/assets/promotoras/dogfight.jpg'
 
 export default function PageHero () {
     return(
         <section className={styles.pagehero}>
-            <Link className={styles.pagehero__bigArticle} href='/articulos/ufc297'>
-                <Image className={styles.pagehero__bigArticle__img} src={ufc297} alt='' />
-                <h2 className={styles.page__title}>UFC 297: ¡Dricus Du Plessis y Raquel Pennington Nuevos Campeones de UFC!</h2>
-                <p className={styles.pagehero__bigArticle__text}>En la noche en que se celebró el UFC 297 en Canadá después de mucho tiempo, tanto la pelea estelar como la coestelar se definieron por decisión...</p>
+            <Link className={styles.pagehero__bigArticle} href='/articulos/ufc298'>
+                <Image className={styles.pagehero__bigArticle__img} src={ufc298} alt='' />
+                <h2 className={styles.page__title}>UFC 298: ¡Ilia Topuria Noquea a Volkanovski y es el Nuevo Campeón de UFC!</h2>
             </Link>
             <Link className={styles.pagehero__article} href='/articulos/ufc300'>
                 <Image className={styles.pagehero__article__img} src={ufc300} alt='' />
