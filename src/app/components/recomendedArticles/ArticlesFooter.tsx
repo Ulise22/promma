8cefd1cb01625a2477a1323c496f4c97f7223bf7
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-02-20 10:23:31 -0300

    added 4 article to the recommended articles component

--- src/app/components/recomendedArticles/ArticlesFooter.tsx
+++ src/app/components/recomendedArticles/ArticlesFooter.tsx
@@ -3,9 +3,13 @@ import styles from './ArticlesFooter.module.css'
 import Image from 'next/image'
 /* IMAGES */
 import ufc300 from '@/assets/articulos/previews/ufc300.jpg'
+import ufc298 from '@/assets/articulos/heros/ufc298_hero.webp'
+import ufc297 from '@/assets/articulos/heros/ufc297_hero.jpg'
 import mostPPV from '@/assets/articulos/previews/mostppv.jpg'
 import top5 from '@/assets/articulos/previews/ufc-goats.webp'
 import khabibVferguson from '@/assets/articulos/previews/khabibVferguson.jpg'
+import dwt2 from '@/assets/articulos/previews/dogfight2.jpg'
+import bestStyle from '@/assets/articulos/previews/mejores-estilos_preview.jpg'
 
 export default function ArticlesFooter () {
     return(
@@ -15,6 +19,14 @@ export default function ArticlesFooter () {
                 <Link className={styles.articlePreview} href='/articulos/ufc300'>
                     <Image className={styles.articlePreview__image} src={ufc300} alt='UFC 300' />
                     <h4 className={styles.articlePreview__title}>UFC 300: Todo los combates confirmados hasta ahora</h4>
+                </Link>
+                <Link className={styles.articlePreview} href='/articulos/ufc298'>
+                    <Image className={styles.articlePreview__image} src={ufc298} alt='' />
+                    <h4 className={styles.articlePreview__title}>UFC 298: Ilia Topuria Noquea a Volkanovski y es Nuevo Campeón de UFC</h4>
+                </Link>
+                <Link className={styles.articlePreview} href='/articulos/ufc297'>
+                    <Image className={styles.articlePreview__image} src={ufc297} alt='' />
+                    <h4 className={styles.articlePreview__title}>UFC 297: Dricus Du Plessis y Raquel Pennington Nuevos campeones de UFC</h4>
                 </Link>
                 <Link className={styles.articlePreview} href='/articulos/most-ppv'>
                     <Image className={styles.articlePreview__image} src={mostPPV} alt='Dustin Poirier vs Conor McGregor' />
@@ -27,6 +39,14 @@ export default function ArticlesFooter () {
                 <Link className={styles.articlePreview} href='/articulos/khabib-ferguson'>
                     <Image className={styles.articlePreview__image} src={khabibVferguson} alt='Khabib vs ferguson' />
                     <h4 className={styles.articlePreview__title}>Khabib vs Ferguson: La pelea maldita que nunca se pudo hacer</h4>
+                </Link>
+                <Link className={styles.articlePreview} href='/articulos/dogfight-wild-tournament-2'>
+                    <Image className={styles.articlePreview__image} src={dwt2} alt='Dogfight Wild Tournament' />
+                    <h4 className={styles.articlePreview__title}>Lo Mejor de Dogfight Wild Tournament, el Evento de Combate Más Loco Organizado por Jordi Wild</h4>
+                </Link>
+                <Link className={styles.articlePreview} href='/articulos/mejores-estilos-mma'>
+                    <Image className={styles.articlePreview__image} src={bestStyle} alt='Jiu Jitsu' />
+                    <h4 className={styles.articlePreview__title}>Las Artes Marciales Más Efectivas para las MMA</h4>
                 </Link>
             </article>
         </section>
