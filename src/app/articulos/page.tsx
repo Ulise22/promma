e425bf9b43da09703511525ee87e6987906f10ea
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-04-30 17:42:53 -0300

    krav maga and marisuz pudzianowski article done

--- src/app/articulos/page.tsx
+++ src/app/articulos/page.tsx
@@ -1,9 +1,9 @@
 import styles from './articulos.module.css'
 import PageHero from './components/PageHero'
 import ArticlePreview from '@/app/articulos/components/ArticlePreview'
+import Noticias from '../components/homepage/Noticias'
 /* IMAGES */
-import dwt2 from '@/assets/promotoras/dogfight.jpg'
-import ufc298 from '@/assets/articulos/previews/10-20/ufc298_preview.jpg'
+/* 0-10 */
 import conorVkhabib from '@/assets/peleadores__images/peleadores__hero.jpg'
 import mostppv from '@/assets/articulos/previews/0-10/mostppv.jpg'
 import khabibVferguson from '@/assets/articulos/previews/0-10/khabibVferguson.jpg'
@@ -11,37 +11,36 @@ import cortePeso from '@/assets/articulos/previews/0-10/corte-peso.jpg'
 import texeira from '@/assets/articulos/previews/0-10/glover_teixeira.webp'
 import goats from '@/assets/articulos/previews/0-10/ufc-goats.webp'
 import bestStyles from '@/assets/articulos/previews/0-10/mejores-estilos_preview.jpg'
-import ufc297 from '@/assets/articulos/previews/0-10/ufc297.jpg'
-import ufc300 from '@/assets/articulos/previews/0-10/ufc300.jpg'
-import queSonMma from '@/assets/articulos/previews/10-20/que-son-mma_preview.jpg'
 import GoatFemUfc from '@/assets/leyendas/(peleadores)/0-10/amanda-nunes/amanda_card.jpg'
+/* 10-20 */
+import queSonMma from '@/assets/articulos/previews/10-20/que-son-mma_preview.jpg'
 import mostLoss from '@/assets/articulos/previews/10-20/most-loss_preview.jpg'
 import origenAM from '@/assets/articulos/previews/10-20/origen-am_preview.jpg'
 import jonesYoungChamp from '@/assets/articulos/previews/10-20/campeon-mas-joven.jpg'
 import masvidalVsAskren from '@/assets/articulos/previews/10-20/masvidal-vs-askren_preview.jpeg'
 import IzziVsPereira from '@/assets/articulos/previews/10-20/izzy-vs-pereira_preview.jpg'
-import rivalsIlia from '@/assets/articulos/insideArticle/ilia-topuria_hero.jpg'
+/* 20-30 */
 import karateDefensaPersonal from '@/assets/articulos/previews/20-30/karate-defensa-personal_preview.jpg'
 import mostProFights from '@/assets/articulos/previews/20-30/most-pro-fights.jpg'
 import allDoubleChamps from '@/assets/articulos/previews/20-30/dobles-campeones.jpg'
-import ufc299 from '@/assets/articulos/previews/10-20/ufc299_preview.jpg'
-import comebackMcGregor from '@/assets/articulos/previews/20-30/conor-mcgregor_volverapelear.jpg'
-import ufc300EarlyPrelims from '@/assets/articulos/previews/20-30/ufc300_early-prelims.jpg'
-import Noticias from '../components/homepage/Noticias'
+import kravmaga from '@/assets/articulos/previews/20-30/kravmaga-preview.jpg'
+import mariusz from '@/assets/articulos/previews/20-30/mariusz-pudzianowski-preview.jpg'
 
 export default function Articulos () {
     return(
         <main className={styles.articulos}>
             <PageHero />
             <section className={styles.articles__container}>
+                <ArticlePreview img={mariusz} url='/articulos/mariusz-pudzianowski' title='El Polaco Que Fue 5 Veces el Hombre Más Fuerte del Mundo, y que con 45 Años Noquea a sus Rivales, Mariusz Pudzianowski' author={null} text='La historia del hombre nacido en Polonia, Mariusz Pudzianowski, que luego de haber desarrollado su carrera como Strongman, siendo elegido el hombre más fuerte del mundo en 5 oportunidades, se mudó a las MMA, donde sorprendió a todos con su impenente físico y su habilidad para noquear rivales con más experiencia que él.' date='30/04/2024' />
+                <ArticlePreview img={kravmaga} url='/articulos/sirve-el-krav-maga' title='¿Sirve el Krav Maga en un combate real?' author={null} text='La razón por la que muchos peleadores profesionales consideran que el Krav Maga y otro tipo de estilos de defensa personal NO sirven para una pelea callejera y para las MMA.' date='30/04/2024' />
                 <ArticlePreview img={allDoubleChamps} url='/articulos/dobles-campeones-de-ufc' title='Todos los Dobles Campeones de UFC' author={null} text='¿Quiénes componen el selecto grupo de los campeones de 2 categorías de peso diferentes en la ufc?' date='01/04/2024' />
                 <ArticlePreview img={mostProFights} url='/articulos/mas-peleas-profesionales' title='El Peleador de MMA con más Peleas y su Oscuro Final' author={null} date='24/03/2024' text='La historia detrás de Travis Fulton, el luchador con más peleas profesionales en MMA y que se suicidó en la carcel en 2021.' />
-                <ArticlePreview img={GoatFemUfc} url='/articulos/mejores-peleadoras-ufc' title='Las Mejores Peleadoras de la Historia de la UFC' author={null} date={null} text='El listado de las mejores peleadoras de la historia en haber peleado en la comapañía de Dana White.' />
-                <ArticlePreview img={mostLoss} url='/articulos/mas-peleas-perdidas-ufc' title='¿Quién es el Peleador Que Más Peleas Perdió de Forma Consecutiva?' author={null} date={null} text='Los peleadores con el poco honroso récord de ser quienes más peleas consecutivas perdieron en la historia de la UFC.' />
                 <Noticias />
             </section>
             <h2 className={styles.articulos__title}>Artículos</h2>
             <section className={styles.articles__container}>
+                <ArticlePreview img={GoatFemUfc} url='/articulos/mejores-peleadoras-ufc' title='Las Mejores Peleadoras de la Historia de la UFC' author={null} date={null} text='El listado de las mejores peleadoras de la historia en haber peleado en la comapañía de Dana White.' />
+                <ArticlePreview img={mostLoss} url='/articulos/mas-peleas-perdidas-ufc' title='¿Quién es el Peleador Que Más Peleas Perdió de Forma Consecutiva?' author={null} date={null} text='Los peleadores con el poco honroso récord de ser quienes más peleas consecutivas perdieron en la historia de la UFC.' />
                 <ArticlePreview img={conorVkhabib} url='/articulos/conor-vs-khabib' title='Conor McGregor vs Khabib Nurmagomedov: ¿Qué pasó realmente?' author={null} date={null} text='La historia detrás de todo el beef que hubo alrededor de estos 2 peleadores, y los conflictos que hubo después de su pelea.' />
                 <ArticlePreview img={masvidalVsAskren} url='/articulos/ko-mas-rapido-ufc' title='El KO Más Rápido en la Historia de la UFC' author={null} date={null} text='La historia detrás de Jorge Masvidal, Ben Askren y como el enfrentamiento entre estos 2 terminó con el KO más rápido en la historia de la UFC.' />
                 <ArticlePreview img={mostppv} url='/articulos/most-ppv' title='Top 5: Las peleas de MMA que más PPV vendieron' author={null} date={null} text='El listado de las peleas que más vendieron en la historia de la UFC, dominada principalmente por Conor McGregor.' />
