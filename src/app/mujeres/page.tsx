fe044bea67c0596b01bc411b97acec1e6a45a22a
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-06-11 12:04:10 -0300

    rose namajumas article done

--- src/app/mujeres/page.tsx
+++ src/app/mujeres/page.tsx
@@ -34,6 +34,9 @@ export default function Mujeres () {
                     <Link href='/mujeres/holly-holm' className={`${styles.peleadores__card} ${styles.peleadores__card_holly}`}>
                         <h3 className={styles.peleadores__card__name}>Holly Holm</h3>
                     </Link>
+                    <Link href='/mujeres/rose-namajumas' className={`${styles.peleadores__card} ${styles.peleadores__card_namajumas}`}>
+                        <h3 className={styles.peleadores__card__name}>Rose Namajumas</h3>
+                    </Link>
                 </article>
                 <h2 className={styles.peleadores__subtitle}>Campeonas de UFC</h2>
                 <article className={styles.peleadores__container}>
