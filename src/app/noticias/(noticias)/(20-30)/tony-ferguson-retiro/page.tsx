315722d78c9f6fd050753677a378239fe77096f4
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-07-27 12:50:45 -0300

    tony ferguson retiro article done

---
+++ src/app/noticias/(noticias)/(20-30)/tony-ferguson-retiro/page.tsx
@@ -0,0 +1,45 @@
+import styles from '@/app/articulos/components/article.module.css'
+import ArticleHero from '@/app/articulos/components/ArticleHero'
+import AsideChamps from '@/app/components/asides/AsideChamps'
+import Link from 'next/link'
+import type { Metadata } from 'next'
+import Image from 'next/image'
+/* Images */
+import hero from '@/assets/noticias/heros/20-30/ferguson_retiro-hero.jpg'
+
+export const metadata: Metadata = {
+    title: 'Tony Ferguson Retiro',
+    description: '',
+    openGraph: {
+        title: 'Tony Ferguson Retiro',
+        description: '',
+        url: 'https://fullmma.org/noticias/tony-ferguson-retiro'
+    }
+}
+
+export default function FergusonRetiro () {
+    return(
+        <main>
+            <ArticleHero title='¿Tony Ferguson Se Retira Luego de su Pelea con Michael Chiesa?' subtitle='El Cucuy se enfrentará al estadounidense Michael Chiesa en agosto, pero ¿será esta su última pelea?' image={hero} date='2024-07-27' author={null} />
+            <section className={styles.article__container}>
+                <article className={styles.article}>
+                    <h2>Tony Ferguson vs Michael Chiesa</h2>
+                    <p>El próximo 3 de agosto, <Link href='/peleadores/tony-ferguson'>Tony Ferguson</Link> y su compatriota <b>Michael Chiesa</b> se enfrentarán en el fightnight de la <Link href='/articulos/ufc'>UFC</Link> organizado en Abu Dhabi, en una pelea que llevará a Ferguson a subir de división hasta las 170lbs, en peso wélter, y por diferentes motivos creemos que esta sí puede ser la pelea del retiro del Cucuy. </p>
+                    <p>La primera de las razones es porque se enfrentará a un rival que tiene muchas posibilidades de ganarle, ya que es un peleador unos cuantos años más joven, que a pesar de venir de 3 derrotas consecutivas, es alguien que jamás fue noqueado, y que a pesar de que también peleaba originalmente en la división de peso ligero, ya lleva bastante más experiencia y peleas luchando contra pesos wélter.</p>
+                    <p>La segunda de las razones es porque así lo afirmó el mismo Dana White, presidente y dueño de la UFC en el anuncio de la pelea, dejando caer la idea de que esta puede ser su última pelea.</p>
+                    <blockquote className={`${styles.article__twitter__quote} twitter-tweet`}><p lang="en" dir="ltr">The main card for <a href="https://twitter.com/hashtag/UFCAbuDhabi?src=hash&amp;ref_src=twsrc%5Etfw">#UFCAbuDhabi</a> is set!!!<a href="https://twitter.com/VisitAbuDhabi?ref_src=twsrc%5Etfw">@VisitAbuDhabi</a> | <a href="https://twitter.com/InAbuDhabi?ref_src=twsrc%5Etfw">@InAbuDhabi</a> | <a href="https://twitter.com/hashtag/InAbuDhabi?src=hash&amp;ref_src=twsrc%5Etfw">#InAbuDhabi</a> <a href="https://t.co/yUK7VdaOAc">pic.twitter.com/yUK7VdaOAc</a></p>&mdash; danawhite (@danawhite) <a href="https://twitter.com/danawhite/status/1795829069826969707?ref_src=twsrc%5Etfw">May 29, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js"></script>
+                    <p>Actualmente Ferguson tiene 40 años de edad y lleva acumulada una racha de 7 derrotas consecutivas, compartiendo en el récord de la mayor cantidad de derrotas seguidas en la UFC junto a <b>BJ Penn</b>. Lo cual es particularmente llamativo si tenemos en cuenta que antes de su primer derrota de estas 7, era el peleador con la racha de victorias activa más larga en la historia de la división, con 12. </p>
+                    <p>Sin embargo, lo que preocupa en el caso de Tony no es el hecho de que lleva muchas derrotas consecutivas, sino que pasó de ser un peleador imbatible al que practicamente era imposible de finalizar, a ser alguien que fue noqueado por <Link href='/peleadores/justin-gaethje'>Justin Gaethje</Link> y <Link href='/peleadores/michael-chandler'>Michael Chandler</Link>; además de ser sometido por peleadores como <Link href='/articulos/nate-diaz-historia'>Nate Diaz</Link> que luego de su combate se despidió de la compañía, y por <b>Bobby Green</b>, que es el #15 de la división. La última de estas derrotas fue contra un peleador que ni siquiera estaba ranqueado como lo es <Link href='/peleadores/paddy-pimblett'>Paddy Pimblett</Link>, quien luego de 3 asaltos lo venció por decisión unánime.</p>
+                    <iframe className={styles.article__video} width="560" height="315" src="https://www.youtube.com/embed/ZuLZm6TF8OA?si=J2fqt1A2f2rmaLdX" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <p>Creemos que independientemente de cómo termine esta pelea contra Chiesa, Ferguson debería retirarse luego de este combate (en realidad creemos que se debería haber retirado mucho antes), porque alguien con la carrera de Ferguson ya no tiene nada que demostrar en este deporte, habiendo sido campeón interino de la división de peso ligero, llegando a acumular una racha de 12 victorias consecutivas, enfrentando y venciendo a todo lo que se le puso por delante, es sin dudas uno de los mejores peleadores de la historia de la UFC en la división de peso ligero. El que nunca haya sido campeón indiscutido, se debe más a cuestiones azarosas que a la habilidad del Cucuy.</p>
+                    <p>En caso de perder su siguiente combate, Ferguson se converitiría en soledad, en el peleador con la mayor cantidad de derrotas consecutivas en la historia de la UFC. No tiene sentido para un peleador como él que siga manchando su legado con más peleas, y más cuando es un peleador que practicamente no tiene para ganar. A esta altura cada pelea lo expone a seguir sufriendo daño innecesario que más temprano que tarde va a afectar su salud.</p>
+                    <p>Suponiendo que perdiera su siguiente pelea, ¿qué sentido tiene para un hombre de 40 años, que ya acumularía 8 derrotas seguidas seguir peleando a este nivel? Más cuando claramente se nota que su mejor nivel lo perdió hace tiempo, como es natural que le pase a todos los peleadores llegados a una cierta edad, especialmente en divisiones de peso tan livianas, como lo es la división de peso ligero.</p>
+                    <p>Independientemente de que luego de su última pelea con Paddy, Tony haya dejado ver que no planeaba retirarse, no sé que tanto sentido tenga para él y su equipo seguir compitiendo a esta altura del partido. Hasta el mismo Michael Chiesa reconoce que esta pelea es entre 2 peleadores que se encuentran en el ocaso de su carrera (siendo Chiesa 4 años más joven que Ferguson).</p>
+                    <iframe className={styles.article__video} width="560" height="315" src="https://www.youtube.com/embed/wrI0Y6WVuzY?si=yg7ghGhjUORrrIn9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <p>A menos que se trate de una pelea mega mediática, que va a llenarle los bolsillos tanto a él como a su familia, como lo sería una hipotética pelea contra <Link href='/peleadores/conor-mcgregor'>Conor Mcgregor</Link>, Tony Ferguson no debería aceptar volver a pelear.</p>
+                </article>
+                <AsideChamps />
+            </section>
+        </main>
+    )
+}
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -3,13 +3,14 @@ import Noticias from "../components/homepage/noticias/Noticias";
 import ArticlePreview from '../articulos/components/ArticlePreview';
 /* HERO IMAGES */
 import whittakerVSaliskerov from '@/assets/noticias/previews/10-20/whittaker-vs-aliskerov.jpg'
+import cucuyRetiro from '@/assets/noticias/previews/20-30/ferguson_retiro_preview.jpg'
 import volkNextFight from '@/assets/noticias/previews/20-30/volk_next_fight_preview.jpg'
 import belalOdiado from '@/assets/noticias/previews/20-30/belal_odiado-preview.jpg'
 import amandaVuelve from '@/assets/noticias/previews/20-30/amanda_nunes_vuelve-preview.jpg'
 import ortegaLopes from '@/assets/noticias/previews/10-20/ortega-vs-lopes-preview.jpg'
-import ufc302 from '@/assets/noticias/previews/10-20/ufc302-preview.jpg'
 import jonesEvitaAspinall from '@/assets/noticias/previews/20-30/jones_evita_aspinall-preview.jpg'
 /* 0 - 10 */
+import ufc302 from '@/assets/noticias/previews/10-20/ufc302-preview.jpg'
 import conor from '@/assets/peleadores__images/0-10/conor-mcgregor/conor_mcgregor.jpg'
 import ufc300 from '@/assets/noticias/previews/0-10/ufc300.jpg'
 import ufc300Prelims from '@/assets/noticias/previews/0-10/ufc300_prelims.jpg'
@@ -40,14 +41,14 @@ export default function NoticiasPage () {
                     <p className={styles.page__title}>UFC Fight Night: ¡Whittaker Noquea en el Primer Asalto a Aliskerov! ¡Shara Bullet Noquea a su Rival en el Tercer Asalto!</p>
                     <span>Un evento donde contamos con emocionantes combates como el que tendran Johnny Walker y Volkan Oezdemir abriendo la cartelera principal, la pelea coestelar entre Sergei Pavlovich y Alexander Volkov, y el evento principal protagonizado por Robert Whittaker y Ikram Aliskerov.</span>
                 </Link>
-                <Link className={`${styles.noticiasHero__article} ${styles.normal1}`} href='/noticias/proxima-pelea-volkanovski'>
-                    <Image className={styles.noticiasHero__article__img} src={volkNextFight} alt='' />
+                <Link className={`${styles.noticiasHero__article} ${styles.normal1}`} href='/noticias/tony-ferguson-retiro'>
+                    <Image className={styles.noticiasHero__article__img} src={cucuyRetiro} alt='' />
                     <article className={styles.noticiasHero__article__container}>
-                        <p className={styles.noticiasHero__title}>¿Volkanovski en Peso Ligero o Revancha Contra Ilia Topuria? Próxima Pelea de Alexander Volkanovski</p>
-                        <span>¿Cuál va a ser la próxima pelea de Alexander Volkanovski? ¿Obtendrá una revancha por el título ante Ilia Topuria, o pasará página y se movera a la división de peso ligero para enfrentar a los otros contendientes?</span>
+                        <p className={styles.noticiasHero__title}>¿Tony Ferguson Se Retira Luego de su Pelea con Michael Chiesa?</p>
+                        <span>El Cucuy se enfrentará al estadounidense Michael Chiesa en agosto, pero ¿será esta su última pelea?</span>
                         <div className={styles.noticiasHero__article__info}>
                             <h4>FULLMMA</h4>
-                            <p>08/07/2024</p>
+                            <p>27/07/2024</p>
                         </div>
                     </article>
                 </Link>
@@ -64,13 +65,14 @@ export default function NoticiasPage () {
                 </Link>
             </article>
             <article className={styles.noticiasHero__container}>
+                <ArticlePreview img={volkNextFight} url='/noticias/proxima-pelea-volkanovski' title='¿Volkanovski en Peso Ligero o Revancha Contra Ilia Topuria? Próxima Pelea de Alexander Volkanovski' author={null} text='.¿Cuál va a ser la próxima pelea de Alexander Volkanovski? ¿Obtendrá una revancha por el título ante Ilia Topuria, o pasará página y se movera a la división de peso ligero para enfrentar a los otros contendientes?' date='08/07/2024' />
                 <ArticlePreview img={jonesEvitaAspinall} url='/noticias/jones-esquiva-aspinall' title='¿Jon Jones Está Evitando a Tom Aspinall?' author={null} text='El actual campeón de peso pesado de UFC Jon Jones, ha rechazado pelear con el campeón interino Tom Aspinall a la espera de combatir en su lugar a Stipe Miocic.' date='16/07/2024' />
                 <ArticlePreview img={amandaVuelve} url='/noticias/vuelve-amanda-nunes' title='¿Amanda Nunes Vuelve a Pelear?' author={null} text='La mejor peleadora de la historia de las MMA está retirada, pero ¿Volverá Amanda Nunes al Octagono?' date='25/06/2024' />
                 <ArticlePreview img={ortegaLopes} url='/noticias/ortega-vs-lopes-previa' title='Diego Lopes vs Brian Ortega: Una Pelea Que Puede Revolucionar la División' author={null} text='El próximo sábado 29 de junio se enfrentarán en la pelea coestelar el brasileño Diego Lopes y el estadounidense Brian Ortega en el UFC 303 es una que puede llegar a verse incluso como una eliminatoria por el título.' date='24/06/2024' />
-                <ArticlePreview img={ufc302} url='/noticias/ufc302' title='UFC 302: ¡Islam Makhachev Somete a Poirier en el Último Asalto de la Pelea!' author={null} text='Islam Makhachev somete a Dustin Poirier en lo que hasta ahora parecería ser la última pelea de su carrera. Además Sean Strickland se lleva una esperable victoria contra Paulo Costa por decisión unánime.' date='02/06/2024' />
                 </article>
             <p className={styles.noticiasPage__subtitle}>Todas Las Noticias</p>
             <article className={styles.noticias}>
+                <ArticlePreview img={ufc302} url='/noticias/ufc302' title='UFC 302: ¡Islam Makhachev Somete a Poirier en el Último Asalto de la Pelea!' author={null} text='Islam Makhachev somete a Dustin Poirier en lo que hasta ahora parecería ser la última pelea de su carrera. Además Sean Strickland se lleva una esperable victoria contra Paulo Costa por decisión unánime.' date='02/06/2024' />
                 <ArticlePreview img={chimaevCancel} url='/noticias/khamzat-whittaker-cancelada' title='Whittaker vs Chimaev ¿Cancelada?' author={null} text='A un par de semanas para el enfrentamiento en Arabia Sáudita entre 2 de los mayores contendientes al título de Peso Medio de UFC, pareciera que se cae la pelea.' date='13/06/2024' />
                 <ArticlePreview img={cannonierVSimano} url='/noticias/fight-night-cannonier-imanov' title='UFC Fight Night: ¡Imanov Noquea a Canonnier! ¡Dominick Reyes Gana por Nocaut en el Primer Asalto! y ¡Raul Rosas JR Somete en el Segundo Asalto!' author={null} text='El UFC Fight Night Canonnier vs Imanov, nos dejó 5 nocauts, 1 sumisión y un montón de peleas espectaculares que valen la pena ver, en una cartelera organizada en la ciudad de Loisville en el Estado de Kentucky.' date='09/06/2024' />
                 <ArticlePreview img={conor} url='/noticias/mcgregor-cancela-pelea' title='Conor McGregor vs Chandler: ¿McGregor Cancela la Pelea?' author={null} text='A poco tiempo de que se celebre el UFC 303, McGregor canceló la rueda de prensa que estaba prevista para promocionar el evento, muchas cosas extrañas han sucedido desde entonces que desperteron todo tipo de rumores, ¿pero qué está pasando realmente?' date='07/06/2024' />
