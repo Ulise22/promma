52f447b1c39bd566eb8384276519a0de6dbd7cac
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-08-13 08:38:35 -0300

    change the way the contact form functions in the page

--- src/app/(info)/contacto/page.jsx
+++ src/app/(info)/contacto/page.jsx
@@ -1,32 +1,14 @@
-'use client'
-import {useRef} from 'react'
 import styles from './Contacto.module.css'
-import emailjs from '@emailjs/browser'
 
 export default function Contacto () {
-    const refForm = useRef()
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(refForm)
-
-        const serviceId = 'service_wuk5aye'
-        const templateId = 'template_b3rvs6s'
-
-        const apiKey = '_Do6W25_YstkB0ttk'
-
-        emailjs.sendForm(serviceId, templateId, refForm.current, apiKey)
-          .then(result => console.log(result.text))
-          .catch(error => console.error(error))
-    }
 
     return(
         <main className={styles.contacto}>
-            <form ref={refForm} onSubmit={handleSubmit} className={styles.form}>
+            <form action="https://formsubmit.co/ulimontenegro23@gmail.com" method="POST" className={styles.form}>
                 <h1 className={styles.form__title}>Contactanos</h1>
                 <div className={styles.form__container}>
                     <label className={styles.form__label}>Nombre:</label>
-                    <input className={styles.form__input} required name='nombre' type='text' placeholder='Ej: John Doe' />
+                    <input className={styles.form__input} required name='name' type='text' placeholder='Ej: John Doe' />
                 </div>
                 <div className={styles.form__container}>
                     <label className={styles.form__label}>Email:</label>
@@ -34,7 +16,7 @@ export default function Contacto () {
                 </div>
                 <div className={styles.form__container}>
                     <label className={styles.form__label}>Mensaje:</label>
-                    <textarea className={styles.form__textarea} required name='mensaje' placeholder='Escriba aquí su mensaje' />
+                    <textarea className={styles.form__textarea} required name='textarea' placeholder='Escriba aquí su mensaje' />
                 </div>
                 <button className={styles.form__btn}>Enviar</button>
             </form>
