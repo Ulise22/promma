48fcc840a2654df781f03d522174731e322b02e2
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-08-18 00:51:04 -0300

    Dan hooker fight done

--- src/app/noticias/(noticias)/(30-40)/ufc305/page.tsx
+++ src/app/noticias/(noticias)/(30-40)/ufc305/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function UFC305 () {
     return(
         <main>
-            <ArticleHero title='UFC 305: ¡Dricus Du Plessis vs Israel Adesanya Por el Título de Peso Medio!' subtitle='¡Carlos Prates deja KO a Li Jingliang en el segundo asalto!' image={hero} date='2024-08-18' author={null} />
+            <ArticleHero title='UFC 305: ¡Dricus Du Plessis vs Israel Adesanya Por el Título de Peso Medio!' subtitle='¡Carlos Prates deja KO a Li Jingliang en el segundo asalto! ¡Dan Hooker consigue una victoria ante Mateusz Gamrot!' image={hero} date='2024-08-18' author={null} />
             <section className={styles.article__container}>
                 <article className={styles.article}>
                     <p>Hoy tenemos un nuevo evento numerado de <Link href='/articulos/ufc'>UFC</Link> celebrado en el RAC Arena en Australia, donde tendremos como pelea estelar el ansiado regreso de <Link href='/peleadores/israel-adesanya'>Israel Adesanya</Link>, quien enfrentará al actual campeón de la división de peso medio de la UFC, el sudafricano <Link href='/peleadores/du-plessis'>Dricus Du Plessis</Link> en lo que será su primer defensa del título desde que lo ganara en el <Link href='/noticias/ufc297'>UFC 297</Link> al derrotar a <Link href='/peleadores/sean-strickland'>Sean Strickland</Link>.</p>
@@ -32,8 +32,8 @@ export default function UFC305 () {
                     <h2>Jairzinho Rozenstruik Gana Por Decisión Dividida a Tai Tuivasa</h2>
                     <p>Jairzinho logra una merecida victoria por decisión ante el australiano <b>Tai Tuivasa</b>. En un combate que inició con un primer asalto sin mucha acción, donde ambos peleadores estaban especulando y parecían tener respeto del poder del otro, terminó con un Jairzinho cada vez más dominante y seguro de sí mismo, que logró conectar los mejores golpes de la pelea. </p>
                     <p>Quizás lo polémico del combate, fue que a pesar de que claramente Jaizinho había sido el ganador de al menos los 2 últimos asaltos de forma muy clara, uno de los jueces le concedió un 30-27 a Tai Tuivasa, viéndolo como ganador de los 3 asaltos.</p>
-                    <h2>Mateusz Gamrot vs Dan Hooker</h2>
-                    <p>En proceso...</p>
+                    <h2>Dan Hooker Derrota a Mateusz Gamrot</h2>
+                    <p>En la que definitivamente fue una de las peleas de la noche, el australiano <b>Dan Hooker</b> hizo valer su localía imponiéndose por sobre el polaco <b>Mateusz Gamrot</b> por decisión dividida. La pelea fue una completa guerra y estuvo bastante cerrada, pero el australino hizo méritos suficientes para que 2 de los jueces lo vieran como ganador, logrando su segunda victoria consecutiva y metiéndose en el top #10 de la división de peso ligero.</p>
                     <h2>Kai Kara-France vs Steve Erceg</h2>
                     <p>En proceso...</p>
                     <h2>Dricus Du Plessis vs Israel Adesanya</h2>
