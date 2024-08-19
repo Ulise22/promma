8921f16266fef8bb5bd885d3b7428a135246c190
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-08-19 15:39:13 -0300

    updated the homepage

--- src/app/components/homepage/ArticulosContainer.tsx
+++ src/app/components/homepage/ArticulosContainer.tsx
@@ -8,15 +8,16 @@ import divisionGoats from '@/assets/articulos/previews/30-40/division_goats-prev
 import boxeadorVsMMA from '@/assets/articulos/previews/30-40/boxeador_vs_mma-preview.jpg'
 import pridefc from '@/assets/articulos/previews/30-40/pridefc-preview.jpg'
 import palhares from '@/assets/articulos/previews/30-40/rousimar_palhares-preview.jpg'
-import mejoresPeleasUFC from '@/assets/articulos/previews/20-30/mejores_peleas_ufc-preview.jpg'
 import LeeMurray from '@/assets/articulos/previews/40-50/lee_murray-preview.jpg'
 import mmaVsBoxPeligro from '@/assets/articulos/previews/40-50/box_vs_mma_danger-preview.jpg'
+import khabibPierde from '@/assets/articulos/previews/40-50/khabib_pierde-preview.jpeg'
 
 export default function ArticulosContainer () {
     return(
         <section className={styles.articulos}>
             <h2 className={styles.articulos__title}>Últimos Artículos</h2>
             <article className={styles.articulos__container}>
+                <HomeArticle author='FULLMMA' date='19/08/2024' img={khabibPierde} link='/articulos/khabib-nurmagomedov-pierde' title='La Única Derrota de Khabib Nurmagomedov' description='La historia y el vídeo de un joven Khabib de 17 años llorando después de perder una pelea de sambo en un campeonato ruso.' />
                 <HomeArticle author='FULLMMA' date='23/07/2024' img={LeeMurray} link='/articulos/lee-murray' title='Lee Murray: El Luchador de UFC Que Realizó el Mayor Robo en Reino Unido' description='Lee Murray, luchador de MMA y expeleador de UFC que vivió su vida como si fuera un personaje de GTA, siendo uno de los perpetuadores del mayor robo de dinero en efectivo en la historia de Reino Unido.' />
                 <HomeArticle author='FULLMMA' date='21/07/2024' img={mmaVsBoxPeligro} link='/articulos/boxeo-vs-mma-peligroso' title='Boxeo vs MMA: ¿Cuál Es Más Peligroso?' description='2 de los deportes de combate más practicados en el mundo, pero uno de los 2 cuenta con la peculiaridad de ser más riesgoso durante su práctica para quien lo entrena.' />
                 <HomeArticle author='FULLMMA' date='08/07/2024' img={jonesEscondido} link='/articulos/jon-jones-esconde-usada' title='El Día Que Jon Jones se Escondió en Un Octagono Para No Ser Testeado' description='La curiosa anécdota sobre Jon Jones y cómo pasó el día entero escondido debajo de un octágono con tal de no ser testeado en las pruebas antidopaje.' />
@@ -26,7 +27,6 @@ export default function ArticulosContainer () {
                 <HomeArticle author='FULLMMA' date='20/06/2024' img={boxeadorVsMMA} link='/articulos/boxeador-vs-mma' title='Boxeador vs MMA: Las Peleas Entre Boxeo y MMA' description='Los enfrentamientos entre luchadores de MMA y boxeadores que se han dado en boxeo.' />
                 <HomeArticle author='FULLMMA' date='19/06/2024' img={pridefc} link='/articulos/que-paso-con-pride-fc' title='¿Qué Pasó con Pride FC?' description='Cómo el control de la mafia japonea Yakuza acabó con el reinado de la mayor compañía de MMA de la historia.' />
                 <HomeArticle author='FULLMMA' date='19/06/2024' img={palhares} link='/articulos/peleador-mas-sucio-ufc' title='El Peleador Más Sucio de la Historia Expulsado de la UFC' description='Rousimar Palhares, el luchador más sucio de la historia de las MMA que por su comportamiento dentro del octagono fue expulsado de la UFC.' />
-                <HomeArticle author='FULLMMA' date='18/06/2024' img={mejoresPeleasUFC} link='/articulos/mejores-peleas-ufc' title='Las Mejores Peleas de UFC de la Historia' description='El listado de las mejores peleas de la historia, basado en los World MMA Awards que premian a la mejor pelea del año desde 2008.' />
             </article>
         </section>
     )
--- src/app/components/homepage/noticias/Noticias.tsx
+++ src/app/components/homepage/noticias/Noticias.tsx
@@ -3,6 +3,7 @@ import styles from './noticias.module.css'
 import Image from 'next/image'
 import HomeArticle from './HomeArticle'
 /* IMAGES */
+import danHookerTattoos from '@/assets/noticias/previews/20-30/dan_hooker_tattoos-preview.jpg'
 import cucuyRetiro from '@/assets/noticias/previews/20-30/ferguson_retiro_preview.jpg'
 import jonesEvitaAspinall from '@/assets/noticias/previews/20-30/jones_evita_aspinall-preview.jpg'
 import whittakerVSaliskerov from '@/assets/noticias/previews/10-20/whittaker-vs-aliskerov.jpg'
@@ -14,6 +15,7 @@ export default function Noticias () {
         <section className={styles.noticias}>
             <h2 className={styles.noticias__title}>Últimas Noticias</h2>
             <article className={styles.noticias__container}>
+                <HomeArticle img={danHookerTattoos} author='FULLMMA' date='17/08/2024' link='/noticias/dan-hooker-tattoos' title='Los tatuajes de Dan Hooker y su significado' description='Cómo la estrella de la UFC Dan Hooker pasó de no tener ningún tatuaje a llenarse el cuerpo de tinta y el significado detrás de sus tatuajes.'/>
                 <HomeArticle img={cucuyRetiro} author='FULLMMA' date='27/07/2024' link='/noticias/tony-ferguson-retiro' title='¿Tony Ferguson Se Retira Luego de su Pelea con Michael Chiesa?' description='El Cucuy se enfrentará al estadounidense Michael Chiesa en agosto, pero ¿será esta su última pelea?'/>
                 <HomeArticle img={jonesEvitaAspinall} author='FULLMMA' date='16/07/2024' link='/noticias/jones-esquiva-aspinall' title='¿Jon Jones Está Evitando a Tom Aspinall?' description='El actual campeón de peso pesado de UFC Jon Jones, ha rechazado pelear con el campeón interino Tom Aspinall a la espera de combatir en su lugar a Stipe Miocic.'/>
                 <HomeArticle img={volkNextFight} author='FULLMMA' date='08/07/2024' link='/noticias/proxima-pelea-volkanovski' title='¿Volkanovski en Peso Ligero o Revancha Contra Ilia Topuria? Próxima Pelea de Alexander Volkanovski' description='¿Cuál va a ser la próxima pelea de Alexander Volkanovski? ¿Obtendrá una revancha por el título ante Ilia Topuria, o pasará página y se movera a la división de peso ligero para enfrentar a los otros contendientes?'/>
