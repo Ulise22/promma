3967e81e284b11651ee7d13391cbcec4247a0b25
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-09-15 02:06:54 -0300

    valentina shevchenko fight done

--- src/app/components/homepage/HomeHero.tsx
+++ src/app/components/homepage/HomeHero.tsx
@@ -11,8 +11,8 @@ export default function HomeHero () {
         <section className={styles.homehero}>
             <Link className={styles.homehero__bigArticle} href='/covertura-ufc/ufc306'>
                 <Image className={styles.homehero__bigArticle__img} src={ufc306} alt='' />
-                <h2 className={styles.home__title}>UFC 306: ¡Raúl Rosas Jr. Logra La Primer Victoria de la Noche en La Esfera! ¡Esteban Ribovics Gana en un Peleón al Mexicano Daniel Zellhuber!</h2>
-                <span>¡Ronaldo Rodríguez Gana por Decisión ante Ode Osbourne! ¡Diego Lopes Derrota a Brian Ortega por Decisión Unánime!</span>
+                <h2 className={styles.home__title}>UFC 306: ¡Valentina Shevchenko Vuelve a Ser Campeona al Derrotar Por Decisión a Alexa Grasso!  ¡Esteban Ribovics Gana en un Peleón al Mexicano Daniel Zellhuber!</h2>
+                <span>¡Ronaldo Rodríguez Gana por Decisión ante Ode Osbourne! ¡Diego Lopes Derrota a Brian Ortega por Decisión Unánime! ¡Raúl Rosas Jr. Logra La Primer Victoria de la Noche en La Esfera!</span>
             </Link>
             <Link className={`${styles.homehero__article} ${styles.normal1}`} href='/articulos/shara-magomedov-ojo'>
                 <Image className={styles.homehero__article__img} src={magomedovOjo} alt='' />
--- src/app/covertura-ufc/(ufc)/(0-15)/ufc306/page.tsx
+++ src/app/covertura-ufc/(ufc)/(0-15)/ufc306/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function UFC306 () {
     return(
         <main>
-            <ArticleHero title="UFC 306: ¡Raúl Rosas Jr. Logra La Primer Victoria de la Noche en La Esfera! ¡Esteban Ribovics Gana en un Peleón al Mexicano Daniel Zellhuber!" subtitle='¡Ronaldo Rodríguez Gana por Decisión ante Ode Osbourne! ¡Diego Lopes Derrota a Brian Ortega por Decisión Unánime! ' image={hero} date='2024-09-15' author={null} />
+            <ArticleHero title="UFC 306: ¡Valentina Shevchenko Vuelve a Ser Campeona al Derrotar Por Decisión a Alexa Grasso! ¡Esteban Ribovics Gana en un Peleón al Mexicano Daniel Zellhuber!" subtitle='¡Ronaldo Rodríguez Gana por Decisión ante Ode Osbourne! ¡Diego Lopes Derrota a Brian Ortega por Decisión Unánime! ¡Raúl Rosas Jr. Logra La Primer Victoria de la Noche en La Esfera!' image={hero} date='2024-09-15' author={null} />
             <section className={styles.article__container}>
                 <article className={styles.article}>
                     <p>Tenemos un tremendo evento de UFC esta noche, por motivo del día de la independencia de México en La Esfera de Las Vegas. Con la pelea títular de peso gallo entre <Link href='/peleadores/sean-omalley'>Sean O&apos;Malley</Link> y <Link href='/peleadores/merab-dvalishvili'>Merab Dvalishvili</Link> como pelea estelar. Además, la pelea coestelar no se queda atrás, trayendonos el final de la trilogía entre la campeona mexicana <Link href='/mujeres/alexa-grasso'>Alexa Grasso</Link> y la leyenda <Link href='/mujeres/valentina-shevchenko'>Valentina Shevchenko</Link>.</p>
@@ -36,7 +36,8 @@ export default function UFC306 () {
                     <p>El nacido en Brasil se lleva la victoria por decisión unánime en un desempeño impresionante, donde supo dominar al estadounidense de principio a fin, estando cerca de la finalización por nocaut en el priemr asalto. </p>
                     <p>De esta forma, con esta victoria ante el número #3 del peso pluma, <Link href='/peleadores/diego-lopes'>Diego Lopes</Link> se posiciona como un claro contendiente al título para enfrentar al ganador del combate entre <Link href='/peleadores/ilia-topuria'>Ilia Topuria</Link> y <Link href='/peleadores/max-holloway'>Max Holloway</Link>.</p>
                     <h2>Alexa Grasso vs Valentina Shevchenko</h2>
-                    <p>En Proceso...</p>
+                    <p>Resurge <Link href='/mujeres/valentina-shevchenko'>Valentina Shevchenko</Link> y vuelve a convertirse en campeona, luego de dominar durante los 5 asaltos en el suelo a la mexicana que nada pudo hacer ante el dominio de la peruana.</p>
+                    <p>Si bien es verdad que <Link href='/mujeres/alexa-grasso'>Alexa Grasso</Link> tuvo más intentos de sumisión, fue Valentina quien llevó el control de la pelea durante los 5 asaltos.</p>
                     <h2>Sean O&apos;Malley vs Merab Dvalishvili</h2>
                     <p>En Proceso...</p>
                 </article>
