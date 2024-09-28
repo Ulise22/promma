d6de1356494c43ba8bd3800bb55e8cf01c8e6ff2
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-09-28 14:24:15 -0300

    ailín perez fight done

--- src/app/covertura-ufc/(fight-night)/(0-15)/fight-night-moicano-saintdenis/page.tsx
+++ src/app/covertura-ufc/(fight-night)/(0-15)/fight-night-moicano-saintdenis/page.tsx
@@ -28,8 +28,9 @@ export default function MoicanoSaintDenis () {
                     <p>En esta tremenda pelea que nos regalaron el español y el mexicano, los jueces finalmente terminaron por decantarse por Dani Barez, quien se llevó la pelea por decisión unánime. </p>
                     <p>Durante el primer asalto el español fue el claro dominador del combate, estando cerca de noquera con una tremenda mano derecha que llevó a su rival al piso. En el segundo asalto la pelea estuvo más cerrado, con un Altamirano que conectó más golpes, pero que sufrió 2 knockdowns por parte del español que lo derribaron al piso. </p>
                     <p>En el tercer asalto el claro dominador fue el peleador mexicano, pero que ya habiendo perdido los 2 asaltos anteriores, terminó por perder la pelea. Sin embargo ambos peleadores nos ofrecieron un lindo espectáculo.</p>
-                    <h2>Ailín Pérez vs Daria Zhelezniakova</h2>
-                    <p>Es Proceso...</p>
+                    <h2>Ailín Pérez Somete a Daria Zhelezniakova en el Primer Asalto</h2>
+                    <p>La argentina <Link href='/mujeres/ailin-perez'>Ailín Pérez</Link> se luce en la pelea y consigue una tremenda sumisión con un triángulo de brazo en el primer asalto que le otorga la victoria. </p>
+                    <p>Con esta victoria, Ailín consigue su cuarta victoria consecutiva, siendo la única peleadora de Argentina ranqueada en el top #15 de su división. </p>
                     <h2>Nassourdine Imavov vs Brendan Allen</h2>
                     <p>En Proceso...</p>
                     <h2>Renato Moicano vs Benoit Saint Denis</h2>
