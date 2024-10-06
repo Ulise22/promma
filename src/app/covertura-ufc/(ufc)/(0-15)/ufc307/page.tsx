f18bc26089c35677a358e46027a0ca453fc28c1a
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-10-05 23:05:06 -0300

    updated last fight

--- src/app/covertura-ufc/(ufc)/(0-15)/ufc307/page.tsx
+++ src/app/covertura-ufc/(ufc)/(0-15)/ufc307/page.tsx
@@ -24,9 +24,10 @@ export default function UFC307 () {
             <section className={styles.article__container}>
                 <article className={styles.article}>
                     <p>En esta noche de UFC en Salt City tenemos una tremenda cartelera con varias peleas que vale la pena ver, siendo este evento encabezado por la pelea del título entre el campeón de los semipesados <Link href='/peleadores/alex-pereira'>Alex Pereira</Link> y el retador <b>Khalil Rountree Jr.</b> en una pelea que promete acción. Además, como cartelera co-estelar, tenemos la disputa por el título de peso gallo femenino entre la campeona <Link href='/mujeres/raquel-pennington'>Raquel Pennington</Link> y la retadora <Link href='/mujeres/julianna-pena'>Julianna Peña</Link>. </p>
-                    <h2>Stephen Thompson Cae Noqueado Ante Joaquinb Buckley en el Tercer Asalto</h2>
+                    <h2>Stephen Thompson Cae Noqueado Ante Joaquin Buckley en el Tercer Asalto</h2>
                     <p>En lo que era la última pelea de las preliminares, <b>Joequin Buckley</b> logra una importantísima victoria por KO ante el veterano <b>Stephen Thompson</b>, consiguiendo con esta su quinto victoria consecutiva en la división de peso wélter. </p>
                     <p>Para asegurarse de que no queden dudas, en una pelea que estaba siendo cerrada y que parecía que iba a terminar en la decisión de los jueces, el estadounidense decidió tomar acción para que no queden dudas de su victoria, conectando una mano en el aire que mandó a dormir a su rival, consiguiendo de esta manera la victoria por la vía del nocaut en el tercer asalto. </p>
+                    <p>Por su parte, &quot;Wonderboy&quot;, con esta consigue su segunda derrota consecutiva, luego de haber perdido en diciembre del año pasado ante <Link href='/peleadores/shavkat-rakhmonov'>Shavkat Rakhmonov</Link>. Para peor, con esta nueva derrota, se puede decir que de sus últimas 5 peleas, sólo fue capaz de ganar 1, ante <b>Kevin Holland</b> que peleara en unos momentos, después perdió ante <b>Gilbert Burns</b>, ante el actual campeón <Link href='/peleadores/belal-muhammad'>Belal Muhammad</Link>, ante el mencionado Rakhmonov, y ahora ante <b>Joaquin Buckley</b>.</p>
                     {/* <h2>Kayla Harrison vs Ketlen Vieira</h2>
                     <p></p>
                     <h2>Roman Dolidze vs Kevin Holland</h2>
