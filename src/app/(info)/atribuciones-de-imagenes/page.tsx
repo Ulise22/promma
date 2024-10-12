0dc99e9edb93c173915c465977124fc1f38af22d
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-10-12 19:25:39 -0300

    deleted low value articles

--- src/app/(info)/atribuciones-de-imagenes/page.tsx
+++ src/app/(info)/atribuciones-de-imagenes/page.tsx
@@ -11,8 +11,8 @@ export default function Atribuciones () {
                 <li>&quot;Benoit Saint Denis 2023.png&quot; de <b>ZACK</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 3.0</li>
                 <li>&quot;Sean O&apos;Malley 2018.png&quot; de <b>Optic Gaming</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 3.0</li>
                 <li>&quot;Belal Muhammad UFC.jpg&quot; de <b>Inaquout</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution-Share Alike 4.0</li>
-                <li>&quot;Israel Adesanya UFC 271.jpg&quot; de <b>MMAnytt</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 3.0</li>
                 <li>&quot;Khalil Rountree, Jr.. (cropped).jpg&quot; de <b>Slsinc</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution-Share Alike 4.0</li>
+                {/* Mujeres */}
                 <li>&quot;Alexa Grasso 2023 03.png&quot; de <b>Alexa Lopez Col</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 3.0</li>
                 <li>&quot;Kayla Harrison Rio 2016.jpg&quot; de <b>Agência Brasil Fotografias</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 2.0</li>
                 <li>&quot;Holly Holm at UFC 246.jpg&quot; de <b>MMAnytt</b>, disponible en Wimedia Commons bajo la licencia Creative Commons Attribution 3.0</li>
