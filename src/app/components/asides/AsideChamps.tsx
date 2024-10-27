325684d1582839386c6f2f01824119e2236eb1c0
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-10-27 18:30:26 -0300

    reeplaced sean o'mallet with merab dvalishvili in the champs aside

--- src/app/components/asides/AsideChamps.tsx
+++ src/app/components/asides/AsideChamps.tsx
@@ -4,7 +4,7 @@ import styles from './asideChamps.module.css'
 /* IMAGES */
 import makhachev from '@/assets/peleadores__images/10-20/islam-makhachev/islam__makhachev.jpg'
 import duPlessis from '@/assets/peleadores__images/0-10/dricus-du-plessis/du-plessis_card.jpg'
-import omalley from '@/assets/peleadores__images/20-30/sean-omalley/sean__omalley.jpg'
+import dvalishvili from '@/assets/peleadores__images/20-30/merab-dvalishvili/merab_dvalishvili-card.jpg'
 import pereira from '@/assets/peleadores__images/0-10/alex-pereira/pereira_sparring2.jpg'
 import belal from '@/assets/peleadores__images/20-30/belal-muhammad/belal-card.jpg'
 import topuria from '@/assets/peleadores__images/10-20/ilia-topuria/illia__topuria.jpg'
@@ -25,9 +25,9 @@ export default function AsideChamps () {
                 <Image className={styles.campeones__image} src={duPlessis} alt='Dricus Du Plessis' />
                 <h4 className={styles.campeones__name}>Dricus Du Plessis</h4>
             </Link>
-            <Link href='/peleadores/sean-omalley' className={styles.campeones}>
-                <Image className={styles.campeones__image} src={omalley} alt='Sean O&apos;Malley' />
-                <h4 className={styles.campeones__name}>Sean O&apos;Malley</h4>
+            <Link href='/peleadores/merab-dvalishvili' className={styles.campeones}>
+                <Image className={styles.campeones__image} src={dvalishvili} alt='Merab Dvalishvili' />
+                <h4 className={styles.campeones__name}>Merab Dvalishvili</h4>
             </Link>
             <Link href='/peleadores/alex-pereira' className={styles.campeones}>
                 <Image className={styles.campeones__image} src={pereira} alt='Sean O&apos;Malley' />
