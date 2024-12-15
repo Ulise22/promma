b1559bf8220a4b8d443659655fc089fabe897248
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-12-14 23:18:03 -0300

    sean woodson fight done

--- src/app/covertura-ufc/(fight-night)/(0-15)/fight-night-covington-buckley/page.tsx
+++ src/app/covertura-ufc/(fight-night)/(0-15)/fight-night-covington-buckley/page.tsx
@@ -8,12 +8,12 @@ import Image from 'next/image'
 import hero from '@/assets/covertura_ufc/fight_night/0-15/covington-vs-buckley.jpg'
 
 export const metadata: Metadata = {
-    title: 'Moicano vs Saint Denis',
+    title: 'Colby Covington vs Joaquin Buckley',
     description: '',
     openGraph: {
-        title: 'Moicano vs Saint Denis',
+        title: 'Colby Covington vs Joaquin Buckley',
         description: '',
-        url: 'https://fullmma.org/covertura-ufc/fight-night-moicano-saintdenis'
+        url: 'https://fullmma.org/covertura-ufc/fight-night-colvington-buckley'
     }
 }
 
@@ -28,22 +28,25 @@ export default function MoicanoSaintDenis () {
                     <h2 className={styles.article__fightsHierarchy}>Preliminares</h2>
                     <h2>Felipe Lima Derrota a Miles Johns</h2>
                     <p>El peleador brasileño de peso pluma de 26 años logra una gran victoria por decisión unánime, luego de dominar la totalidad del enfrentamiento, ante un Miles Johns que nada pudo hacer ante la cálidad de Lima, quien aprovechando su altura y su alcance, logro conectar más golpes que su rival, casi sin recibir golpes por parte de su rival. </p>
+                    <h2>Sean Woodson Noquea a Fernando Padilla en el Primer Asalto</h2>
+                    <p>El estadounidense logra una increíble victoria por nocaut en el primer asalto, a 2 segundos de que termine, ante el mexicano <b>Fernando Padilla</b>, siendo este el primer nocaut de la noche. </p>
+                    <p>En un enfrentamiento donde se veía a los 2 rivales golpeándose de lo lindo, siendo ambos capaces de conectarse mutuamente, fue finalmente Woodson quien consiguió quedarse con la victoria, al ser capaz de golpear más limpio en la cabeza de su rival, que poco hizo por esquivar o por bloquear los golpes del luchador americano. </p>
                     {/* <h2>Joel Álvarez vs Drakkar Klose</h2>
                     <p></p> */}
                     {/* <h2>Michael Johnson vs Ottman Azaitar</h2>
-                    <p></p>
-                    <h2 className={styles.article__fightsHierarchy}>Cartelera Estelar</h2>
+                    <p></p> */}
+                    {/* <h2 className={styles.article__fightsHierarchy}>Cartelera Estelar</h2>
                     <h2>Navajo Stirling vs Tuco Tokkos</h2>
-                    <p></p>
-                    <h2>Adrian Yanez vs Daniel Marcos</h2>
-                    <p></p>
-                    <h2>Vitor Petrino vs Dustin Jacoby</h2>
-                    <p></p>
-                    <h2>Manel Kape vs Bruno Silva</h2>
-                    <p></p>
-                    <h2>Cub Swanson vs Billy Quarantillo</h2>
-                    <p></p>
-                    <h2>Colby Covington vs Joaquin Buckley</h2>
+                    <p></p> */}
+                    {/* <h2>Adrian Yanez vs Daniel Marcos</h2>
+                    <p></p> */}
+                    {/* <h2>Vitor Petrino vs Dustin Jacoby</h2>
+                    <p></p> */}
+                    {/* <h2>Manel Kape vs Bruno Silva</h2>
+                    <p></p> */}
+                    {/* <h2>Cub Swanson vs Billy Quarantillo</h2>
+                    <p></p> */}
+                    {/* <h2>Colby Covington vs Joaquin Buckley</h2>
                     <p></p> */}
                 </article>
                 <AsideChamps />
