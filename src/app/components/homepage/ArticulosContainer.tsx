2e6f385c69a4dd9e3a712ccdf5491218071421e9
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-01-18 17:15:43 -0300

    updated the homepage

--- src/app/components/homepage/ArticulosContainer.tsx
+++ src/app/components/homepage/ArticulosContainer.tsx
@@ -1,32 +1,32 @@
 import styles from './articulosContainer.module.css'
 import HomeArticle from './noticias/HomeArticle'
 /* IMAGES */
+import joeRoganMA from '@/assets/articulos/0-100/30-40/joe_rogan_bestMA.jpeg'
+import movimientosProhibidosUFC from '@/assets/articulos/0-100/20-30/movimientos_prohibidos_ufc.jpg'
+import luchadoresFamososUFC from '@/assets/articulos/0-100/20-30/luchadores_ufc_mas_famosos.jpg'
+import requisitosEntrarUFC from '@/assets/articulos/0-100/20-30/requisitos_entrar_ufc.jpg'
 import rankingsUFC from '@/assets/articulos/0-100/10-20/ufc_rankings.jpg'
 import salariosUFC from '@/assets/articulos/0-100/10-20/salarios_ufc.jpg'
 import talbottSkateboard from '@/assets/articulos/0-100/10-20/payton_talbott_skateboard.jpg'
 import talbottTattoo from '@/assets/articulos/0-100/10-20/payton_talbott_tattoo.jpg'
 import bestMAForMMA from '@/assets/articulos/0-100/10-20/best_ma_for_mma.jpg'
 import titovsdana from '@/assets/articulos/0-100/0-10/dana_vs_ortiz.jpg'
-import popek from '@/assets/articulos/0-100/50-60/popek_monster.jpg'
-import hollywood from '@/assets/articulos/0-100/50-60/ufc_hollywood.jpg'
-import butterbean from '@/assets/articulos/0-100/50-60/eric_butterbean.jpg'
-import kimboSlice from '@/assets/articulos/0-100/50-60/kimbo_slice.jpg'
 
 export default function ArticulosContainer () {
     return(
         <section className={styles.articulos}>
             <h2 className={styles.articulos__title}>Últimos Artículos</h2>
             <article className={styles.articulos__container}>
+                <HomeArticle author='FULLMMA' date='17/01/2025' img={joeRoganMA} link='/articulos/mejor-arte-marcial-para-pelea-callejera-joe-rogan' title='La Mejor Arte Marcial Para Una Pelea Callejera Según Joe Rogan' description='El comentarista de la UFC Joe Rogan confirma cuál es para él la mejor arte marcial para peleas callejeras. ' />
+                <HomeArticle author='FULLMMA' date='15/01/2025' img={movimientosProhibidosUFC} link='/articulos/movimientos-prohibidos-de-la-ufc' title='Todos Los Movimientos que Están Prohibidos en la UFC: Lo Que Jamás Verás en un Combate' description='La lista de los 28 golpes y movimientos que están prohibidos dentro de la UFC y que por lo tanto jamás verás realizarse, o al menos no deberías ver realizarse. ' />
+                <HomeArticle author='FULLMMA' date='12/01/2025' img={luchadoresFamososUFC} link='/articulos/luchadores-ufc-mas-famosos' title='¿Quienes son los Peleadores de UFC Más Famosos? La lista de los 10 Peleadores con Más Seguidos' description='Compartiremos la lista de los 10 peleadores de UFC con más seguidores en Instagram. Que nos ayudará a descubrir quién es el peleador más famoso de todos en la UFC. ' />
+                <HomeArticle author='FULLMMA' date='12/01/2025' img={requisitosEntrarUFC} link='/articulos/requisitos-para-entrar-a-ufc' title='¿Cómo ser peleador de la UFC? Las 5 formas de entrar a la UFC siendo peleador de MMA.' description='Los requisitos para entrar a la UFC y ser reclutado por la compañía. Las 5 vías por las que Dana White contrata peleadores profesionales.' />
                 <HomeArticle author='FULLMMA' date='31/12/2024' img={rankingsUFC} link='/articulos/como-funciona-el-ranking-ufc' title='Cómo Funciona el Ranking de la UFC: Qué significa ser el #1 de la UFC' description='Te explico cómo funcionan los rankings en la UFC, cuándo se actualizan y cómo se hacen, explicando quienes hacen los rankings y que tienen en cuenta. ' />
                 <HomeArticle author='FULLMMA' date='27/12/2024' img={salariosUFC} link='/articulos/cuanto-gana-un-peleador-de-ufc' title='¿Cuánto le pagan a un peleador de UFC? Los salarios de los peleadores más importantes de MMA' description='Cuánto ganan los peleadores de UFC, desde los novatos, un peleador promedio, un campeón, y cómo sus resultados influyen en el sueldo. ' />
                 <HomeArticle author='FULLMMA' date='26/12/2024' img={talbottSkateboard} link='/articulos/payton-talbott-skateboard' title='Payton Talbott: Cómo el Skateboard Influye en su Carrera en la UFC' description='La influencia que puede tener el skate, junto con otros hobbies, en un peleador de UFC como Payton Talbott. Además te contamos la historia de cómo Payton Talbott conoció a la leyenda del skateboard, Tony Hawk.' />
                 <HomeArticle author='FULLMMA' date='21/12/2024' img={talbottTattoo} link='/articulos/payton-talbott-tattoo' title='¿Qué significan los tatuajes de Payton Talbott?' description='El significado de los tatuajes del peleador de la UFC Payton Talbott. ' />
                 <HomeArticle author='FULLMMA' date='23/09/2024' img={bestMAForMMA} link='/articulos/mejores-artes-marciales-para-mma' title='Las Mejores Artes Marciales Para MMA Según un Peleador Profesional' description='Repasamos cuáles son las mejores artes marciales para una pelea de MMA, según el luchador profesional Jeff Chan.' />
                 <HomeArticle author='FULLMMA' date='14/09/2024' img={titovsdana} link='/articulos/dana-white-vs-tito-ortiz' title='La Pelea de Boxeo Entre Dana White y Tito Ortiz que Finalmente no Ocurrió' description='La historia de rivalidad entre Tito Ortiz y Dana White, y la pelea de boxeo entre estos 2 que se llegó a pactar pero que nunca se dió.' />
-                <HomeArticle author='FULLMMA' date='10/09/2024' img={popek} link='/articulos/popek-monster-mma' title='Popek: El Rapero Polaco y Pelador de MMA Más Loco del Mundo' description='La historia del rapero polaco Popek Monster, el caótico estilo de vida que lleva y su carrera en las artes marciales mixtas.' />
-                <HomeArticle author='FULLMMA' date='10/09/2024' img={hollywood} link='/articulos/peleadores-de-ufc-en-peliculas' title='Top 5 Peleadores de UFC que Aparecieron en Películas de Hollywood' description='La Lista de 5 Campeones de UFC que aparecieron en el cine y televisión, teniendo una actuacion destacada.' />
-                <HomeArticle author='FULLMMA' date='08/09/2024' img={butterbean} link='/articulos/biografia-de-eric-butterbean' title='¿Quién fue Eric Butterbean y por qué era tan famoso?' description='La historia de vida de Eric Butterbean, quien paso de ser un luchador aficionado en concursos de boxeo a convertirse en una estrella internacional en el mundo del boxeo y, eventualmente, en las artes marciales mixtas.' />
-                <HomeArticle author='FULLMMA' date='05/09/2024' img={kimboSlice} link='/articulos/kimbo-slice-historia' title='Kimbo Slice: El rey de los luchadores web' description='La historia de Kevin Ferguson, mejor conocido como Kimbo Slice, y su historia de superación que lo llevó de ser guardia de seguridad de un club nocturno, a ser un luchador profesional.' />
             </article>
         </section>
     )
--- src/app/components/homepage/noticias/Noticias.tsx
+++ src/app/components/homepage/noticias/Noticias.tsx
@@ -3,6 +3,8 @@ import styles from './noticias.module.css'
 import Image from 'next/image'
 import HomeArticle from './HomeArticle'
 /* IMAGES */
+import ufcBillonario from '@/assets/noticias/0-100/20-30/ufc_busca_millonada.jpg'
+import entrenamientoProchazka from '@/assets/noticias/0-100/20-30/prochazka_training.png'
 import previaUFC311 from '@/assets/noticias/0-100/20-30/previa_ufc311.jpg'
 import predicciones2025 from '@/assets/noticias/0-100/20-30/campeones_ufc2025.jpg'
 import invictosUFC from '@/assets/noticias/0-100/20-30/invictos_ufc_2025.jpg'
@@ -11,14 +13,14 @@ import evloevPeleaTopuria from '@/assets/noticias/0-100/20-30/evloev_postfight.j
 import espUFC2025 from '@/assets/noticias/0-100/0-10/peleadores_espanoles_ufc.jpg'
 import kaiAsakura from '@/assets/noticias/0-100/0-10/kai_asakura.jpg'
 import campeonesViejosUFC from '@/assets/noticias/0-100/0-10/campeones_viejos_ufc.jpg'
-import sharaBulletFeb2025 from '@/assets/noticias/0-100/0-10/shara_bullet_feb_2025.jpg'
-import argUFC2024 from '@/assets/noticias/0-100/20-30/argentinos2024.jpg'
 
 export default function Noticias () {
     return(
         <section className={styles.noticias}>
             <h2 className={styles.noticias__title}>Últimas Noticias</h2>
             <article className={styles.noticias__container}>
+                <HomeArticle img={ufcBillonario} author='FULLMMA' date='15/01/2025' link='/noticias/la-ufc-busca-contrato-billonario' title='La UFC Busca un Contrato Multimillonario de Mil Millones, Según Bloomberg, Por Sus Derechos Televisivos' description='Este año 2025 expira el contrato actual de la UFC con ESPN, por lo que la empresa de MMA más grande del planeta estaría buscando aumentar su contrato actual a uno de 10 cifras. '/>
+                <HomeArticle img={entrenamientoProchazka} author='FULLMMA' date='14/01/2025' link='/noticias/como-entrena-jiri-prochazka' title='¿Cómo Entrena Jiri Prochazka? El entrenamiento y preparación mental de Jiri Prochazka que incluye golpear 500 veces un árbol y 3 días encerrado sin luz ni comida para confrontar sus demonios. ' description='El loco y salvaje régimen de entrenamiento al que se somete el excampeón de peso semipesado, con su famosa preparación mental que incluye pasar 3 días encierrado a oscuras en una habitación con sólo agua. '/>
                 <HomeArticle img={previaUFC311} author='FULLMMA' date='10/01/2025' link='/noticias/previa-ufc-311' title='Previa UFC 311: Arman Tsarukyan Busca el Título de Peso Ligero en su Revancha Ante Islam Makhachev' description='¡El equipo Khabib protagonista! En una noche donde los cinturones de peso gallo y peso ligero estarán en juego en el UFC 311, con Merab Dvalishvi haciendo su primer defensa del título ante Umar Nurmagomedov'/>
                 <HomeArticle img={predicciones2025} author='FULLMMA' date='31/12/2024' link='/noticias/predicciones-ufc-2025' title='Predicción UFC: Todos los campeones del 2025' description='Una predicción sobre quienes creo que serán los nuevos campeones para el final del año 2025'/>
                 <HomeArticle img={invictosUFC} author='FULLMMA' date='26/12/2024' link='/noticias/peleadores-invictos-ufc' title='Los Peleadores Invictos de UFC en 2025' description='Los luchadores de UFC que terminaron el 2024 invictos y quienes serán capaces de mantenerlo en el 2025.'/>
@@ -27,8 +29,6 @@ export default function Noticias () {
                 <HomeArticle img={espUFC2025} author='FULLMMA' date='28/11/2024' link='/noticias/luchadores-espanoles-en-ufc' title='Los 5 Peleadores Españoles Actuales de UFC en 2024' description='Los luchadores españoles de UFC que compiten actualmente en la compañía más grande de MMA del mundo.'/>
                 <HomeArticle img={kaiAsakura} author='FULLMMA' date='11/11/2024' link='/noticias/quien-es-kai-asakura' title='¿Quién es Kai Asakura? La Estrella Japonesa Que Debutará Peleando Por el Título de UFC en el UFC 310 en Diciembre' description='El luchador japonés Kai Asakura de 31 años que peleará por el Campeonato de Peso Mosca en su debut en la UFC.'/>
                 <HomeArticle img={campeonesViejosUFC} author='FULLMMA' date='11/11/2024' link='/noticias/5-campeones-mas-viejos-ufc' title='Los 5 Campeones Más Viejos de la UFC: La Lista a la que se Puede Sumar Stipe Miocic' description='El próximo sábado, el luchador estadounidense de 42 años, peleará por el cinturón de peso pesado ante Jon Jones, donde podría convertirse en uno de los campeones más añejos de la historia.'/>
-                <HomeArticle img={sharaBulletFeb2025} author='FULLMMA' date='04/11/2024' link='/noticias/shara-bullet-pelea-febrero-2025' title='¿Shara Magomedov vs Paulo Costa Para Febrero de 2025?' description='¿Cuál será el próximo rival de Shara Bullet en febrero del 2025? ¿Israel Adesanya? ¿Paulo Costa? ¿Michael Venom Page?'/>
-                <HomeArticle img={argUFC2024} author='FULLMMA' date='26/09/2024' link='/noticias/peleadores-de-ufc-argentinos-2024' title='Los 5 Peleadores de UFC Argentinos Que Compiten en 2024' description='Con la llegada de Kevin Vallejos a la UFC, ya son 5 los argentinos que compiten en la UFC. Quiénes son y cómo les va.'/>
             </article>
         </section>
     )
