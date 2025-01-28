25b5cc70c511eee5d75445976539b28cfb174c19
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-01-28 16:19:32 -0300

    new article khabib-disciplina

---
+++ src/app/articulos/(articulos)/(0-100)/(50-60)/khabib-disciplina/page.tsx
@@ -0,0 +1,52 @@
+import styles from '@/app/articulos/components/article.module.css'
+import ArticleHero from '@/app/articulos/components/ArticleHero'
+import AsideChamps from '@/app/components/asides/AsideChamps'
+import Link from 'next/link'
+import type { Metadata } from 'next'
+import Image from 'next/image'
+/* Images */
+import hero from '@/assets/articulos/0-100/50-60/khabib_disciplina.jpg'
+import EndArticle from '@/app/components/EndArticle'
+
+export const metadata: Metadata = {
+    title: 'La Disciplina de Khabib',
+    description: 'Descubre cuáles son las 3 claves para adoptar el increíble nivel de disciplina que tiene Khabib Nurmagomedov, que le ha llevado a cosechar tantos exitos en la UFC.',
+    openGraph: {
+        title: 'La Disciplina de Khabib',
+        description: 'Descubre cuáles son las 3 claves para adoptar el increíble nivel de disciplina que tiene Khabib Nurmagomedov, que le ha llevado a cosechar tantos exitos en la UFC.',
+        url: 'https://fullmma.org/articulos/khabib-disciplina'
+    }
+}
+
+export default function KhabibDisciplina () {
+    return(
+        <main>
+            <ArticleHero title='Las 3 Claves de la disciplina de Khabib Nurmagomedov' subtitle='Los secreto detrás del arrollador éxito y disciplina espectacular de Khabib que supo trasladar a todo su equipo.' image={hero} date='2025-01-28' author={null} updatedDate={null} />
+            <section className={styles.article__container}>
+                <article className={styles.article}>
+                    <p>Cuando uno ve los éxitos que ha cosechado Khabib Nurmagomedov, siendo campeón de peso ligero en UFC, con su victoria ante Conor, ante Poirier y ante Gaethje, retirándose invicto con 29-0, y demostrando una superioridad abrumadora hacia sus rivales, a uno sólo le queda preguntar ¿cómo lo hace? Porque lo impresionante del método Khabib, no son solamente los logros que le trajeron a sí mismo, sino que fue capaz de trasladarlos y convertirlos en logros de sus compañeros y equipo. Con personas como Islam Makhachev, Umar Nurmagomedov o Usman Nurmagomedov siendo ejemplos de esto. Por eso hoy vamos a repasar las claves y secretos detrás de la arrolladora disciplina que fue capaz de construir Khabib Nurmagomedov. </p>
+                    <h2>1. No que te quejes</h2>
+                    <p>Una de las corrientes filosóficas que uno puede rápidamente asociar a campeones como Khabib, es el estoicismo, que el excampeón de peso ligero aplica diariamente e inculca en sus compañeros de gimnasio. </p>
+                    <p>Es por eso que una de las reglas más importantes es la de no quejarse. Nadie te obliga a entrenar, ni a buscar ser un peleador profesional, o un campeón del mundo, eres libre de entrenar con él si es lo que deseas, o sino también tienes la posibilidad de irte a tu casa. Pero lo que Khabib no quiere es que estes para quejarte, si estás en el gimnasio es para entrenar duro, no para quejarte de que estás cansado, o de cualquier otra cosa. </p>
+                    <p>En el canal de YouTube <b>Washed</b>, en el podcast “The Muslim Money Experts”, dijo: <b>“no matamos a nadie, no obligamos a nadie, si no quieres, está bien, vete... incluso ayer, estaba hablando porque era sábado y todos se estaban quejando de que estaban cansados. ¿Qué quieres? Toda la semana estuvimos trabajando duro, si para el final de la semana no estás cansado, ahí debes preocuparte por ti mismo”. </b></p>
+                    <iframe className={styles.article__video} loading='lazy' width="560" height="315" src="https://www.youtube.com/embed/JV9P_-3evpc?si=xGgtmMreytB8l0X1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <p><b>“Es sábado, estuviste trabajando toda la semana tan duro. Por supuesto que vas a estar cansado. Mañana descansas y el lunes vuelves a entrenar. Si no es lo que quieres, vuelve a Daguestán y quédate con tu mamá. Ella te va a dar todos los días un buen desayuno, no tienes que hacer nada, quédate ahí. Pero si quieres venir aquí, no te quejes. Quieres convertirte en el mejor, quieres ser campeón para luego quejarte de que estás cansado, ¿a quién le importa?”</b></p>
+                    <p><b>“Si vas a ver una pelea, sólo te importa la pelea, ves como los peleadores hacen la caminata al octágono, como pelean, y se van. No ves el detrás de escena, no es asunto tuyo, a nadie le importa eso”.</b></p>
+                    <p><b>“Ustedes sólo ven como peleamos y después se van. Y para eso tenemos que entrenar muy duro. Y si no quieres, avísanos y vuelve a Daguestán”. </b></p>
+                    <h2>2. Entrena Duro y Sacrifica el 100%</h2>
+                    <p>Otra cosa que caracteriza la mentalidad del excampeón de peso ligero, es no es una persona que busca equilibrios, o que buscó balancear una vida laboral con su carrera deportiva, o que tenía diferentes intereses además de pelear. Por el contrario, para Khabib, si lo que buscas es ser el mejor en algo, necesitas de depositar el 100% de tu energía y dedicación a ese algo, sin distracciones, no puedes pretender dedicarle el 50% y obtener buenos resultados. Para conseguir el éxito necesitas de sacrificar toda tu vida en esa área en la que buscas sobresalir.</p>
+                    <p>Por lo que, para alcanzar la cima, es necesario repetir la misma rutina durante toda tu vida, renunciando a lo demás. En base a esta experiencia es que está constituido su gimnasio y su equipo. Según él mismo explica: <b>“Tenemos un hotel, tenemos comida, tenemos autos y tenemos un gimnasio. Todos los días, te levantas, entrenas, comes, duermes y repites.”</b></p>
+                    <p><b>“Estuve haciendo lo mismo toda mi vida. Estabilidad, sino tienes estabilidad, si no te recuperas de los entrenamientos, nunca te vas a volver exitoso, en este caso en las artes marciales mixtas. Tienes que entrenar todos los días y hacer lo mismo, sparring, grappling, wrestling, las comidas… todos los días, durante muchos años, necesitas sacrificar todo lo que tienes.”  </b></p>
+                    <h2>3. Descanso y Recuperación</h2>
+                    <p>Finalmente, una de las claves del éxito que alcanzo Khabib y que de igual forma replicaron personas como Islam Makhachev y Usman Nurmagomedov, es la importancia que le dieron al descanso. Si vas a entrenar tan duro, de la forma en que lo hacen estos peleadores, es importante que de igual forma tengas un descanso acorde al esfuerzo que hiciste. </p>
+                    <p>Este punto está muy relacionado con el anterior, puesto que, para el ruso, no puedes estar trabajando o dedicando tu tiempo a otras actividades luego de entrenar. Es importante que el tiempo que no se usa para entrenar, se use para descansar. En el caso de Khabib, su forma preferida de tener descanso es dormir, durmiendo durante la noche y siestas de 2 horas entre los entrenamientos. </p>
+                    <p>Según explica: <b>“Entre los entrenamientos necesitas recuperarte, ¿Dónde esta tu recuperación? La mayoría de la gente recurre a los masajes, a suplementos… Para mí, la forma de recuperarse es dormir, es lo más importante, entre entrenamientos me dedico a dormir. Porque entrenas tan duro durante la mañana, y lo haces a la noche también, necesitas descansar, porque necesitas llegar fresco a los entrenamientos”. </b></p>
+                    <p></p>
+                    <p></p>
+                    <EndArticle />
+                </article>
+                <AsideChamps />
+            </section>
+        </main>
+    )
+}
--- src/app/articulos/page.tsx
+++ src/app/articulos/page.tsx
@@ -55,6 +55,7 @@ import zabit from '@/assets/articulos/0-100/40-50/zabit.jpg'
 import cmpunk from '@/assets/articulos/0-100/40-50/cm_punk.jpg'
 import nicoMontano from '@/assets/articulos/0-100/40-50/nicco_montano.jpg'
 /* 50-60 */ 
+import khabibDisciplina from '@/assets/articulos/0-100/50-60/khabib_disciplina.jpg'
 import hermanosUFC from '@/assets/articulos/0-100/50-60/hermanos_ufc.jpg'
 import llegarAUFC5Pasos from '@/assets/articulos/0-100/50-60/llegar_a_ufc_5pasos.jpg'
 import kimboSlice from '@/assets/articulos/0-100/50-60/kimbo_slice.jpg'
@@ -67,13 +68,14 @@ export default function Articulos () {
         <main className={styles.articulos}>
             <PageHero />
             <section className={styles.articles__container}>
+                <ArticlePreview img={khabibDisciplina} url='/articulos/khabib-disciplina' title='Las 3 Claves de la disciplina de Khabib Nurmagomedov' author={null} text='Los secreto detrás del arrollador éxito y disciplina espectacular de Khabib que supo trasladar a todo su equipo.' date='28/01/2025' />
                 <ArticlePreview img={hermanosUFC} url='/articulos/hermanos-ufc' title='Hermanos Que Pelearon en la UFC' author={null} text='La lista de las mejores parejas de hermanos que pelearon en la UFC ' date='26/01/2025' />
                 <ArticlePreview img={llegarAUFC5Pasos} url='/articulos/como-llegar-a-la-ufc-5-pasos' title='Descubre como llegar a pelear en la UFC en 5 pasos' author={null} text='Convierte tu sueño en realidad: Sigue estos 5 pasos para llegar a la UFC y ser un peleador profesional' date='24/01/2025' />
                 <ArticlePreview img={mikheilKajaia} url='/articulos/mikheil-kajaia' title='¿Quién es Mikheil Kajaia? El luchador olímpico georgiano de 97 kilos y de aspecto más aterrador que existe' author={null} text='La historia detrás del luchador grecorromano nacido en Georgia y nacionalizado serbio. ' date='22/01/2025' />
-                <ArticlePreview img={joeRoganMA} url='/articulos/mejor-arte-marcial-para-pelea-callejera-joe-rogan' title='La Mejor Arte Marcial Para Una Pelea Callejera Según Joe Rogan' author={null} text='El comentarista de la UFC Joe Rogan confirma cuál es para él la mejor arte marcial para peleas callejeras. ' date='17/01/2025' />
             </section>
             <h2 className={styles.articulos__title}>Artículos</h2>
             <section className={styles.articles__container}>
+                <ArticlePreview img={joeRoganMA} url='/articulos/mejor-arte-marcial-para-pelea-callejera-joe-rogan' title='La Mejor Arte Marcial Para Una Pelea Callejera Según Joe Rogan' author={null} text='El comentarista de la UFC Joe Rogan confirma cuál es para él la mejor arte marcial para peleas callejeras. ' date='17/01/2025' />
                 <ArticlePreview img={movimientosProhibidosUFC} url='/articulos/movimientos-prohibidos-de-la-ufc' title='Todos Los Movimientos que Están Prohibidos en la UFC: Lo Que Jamás Verás en un Combate' author={null} text='La lista de los 28 golpes y movimientos que están prohibidos dentro de la UFC y que por lo tanto jamás verás realizarse, o al menos no deberías ver realizarse. ' date='15/01/2025' />
                 <ArticlePreview img={luchadoresFamososUFC} url='/articulos/luchadores-ufc-mas-famosos' title='¿Quienes son los Peleadores de UFC Más Famosos? La lista de los 10 Peleadores con Más Seguidos' author={null} text='Compartiremos la lista de los 10 peleadores de UFC con más seguidores en Instagram. Que nos ayudará a descubrir quién es el peleador más famoso de todos en la UFC.' date='13/01/2025' />
                 <ArticlePreview img={requisitosEntrarUFC} url='/articulos/requisitos-para-entrar-a-ufc' title='¿Cómo ser peleador de la UFC? Las 5 formas de entrar a la UFC siendo peleador de MMA.' author={null} text='Los requisitos para entrar a la UFC y ser reclutado por la compañía. Las 5 vías por las que Dana White contrata peleadores profesionales.' date='12/01/2025' />
