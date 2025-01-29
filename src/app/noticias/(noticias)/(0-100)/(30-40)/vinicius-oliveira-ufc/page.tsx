2b9a1cdd392040ada139d859cf7b934a9953edc7
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-01-29 12:53:23 -0300

    new article vinicius-oliveira-ufc

---
+++ src/app/noticias/(noticias)/(0-100)/(30-40)/vinicius-oliveira-ufc/page.tsx
@@ -0,0 +1,46 @@
+import styles from '@/app/articulos/components/article.module.css'
+import ArticleHero from '@/app/articulos/components/ArticleHero'
+import AsideChamps from '@/app/components/asides/AsideChamps'
+import Link from 'next/link'
+import type { Metadata } from 'next'
+import Image from 'next/image'
+/* Images */
+import hero from '@/assets/noticias/0-100/30-40/vinicius_oliveira.jpg'
+import EndArticle from '@/app/components/EndArticle'
+
+export const metadata: Metadata = {
+    title: 'Vinicius Oliveira UFC',
+    description: "La historia y carrera del peleador de UFC brasileño Vinicius Oliveira. ",
+    openGraph: {
+        title: 'Vinicius Oliveira UFC',
+        description: "La historia y carrera del peleador de UFC brasileño Vinicius Oliveira. ",
+        url: 'https://fullmma.org/noticias/vinicius-oliveira-ufc'
+    }
+}
+
+export default function ViniciusOliveira () {
+    return(
+        <main>
+            <ArticleHero title='¿Quién es Vinicius Oliveira? El brasileño con KOs espectaculares en la UFC' subtitle="La historia y carrera del peleador de UFC brasileño Vinicius Oliveira." image={hero} date='2025-01-29' author={null} updatedDate={null} />
+            <section className={styles.article__container}>
+                <article className={styles.article}>
+                    <p>Este próximo sábado en el UFC Saudi Arabia, tendremos en la cartelera estelar la pelea entre el ruso <b>Said Nurmagomedov</b> (quien nada tiene que ver con <Link href='/leyendas/khabib-nurmagomedov'>Khabib Nurmagomedov</Link>, en caso de que te lo preguntaras), y el brasileño <b>Vinicius Oliveira</b>, de quien vamos a hablar hoy. </p>
+                    <p>A pesar de que en las casas de apuestas, Said Nurmagomedov comienza como el favorito, lo cierto es que no hay que hacer de menos al brasileño. Primero, porque la diferencia no es tan grande, y segundo porque la habilidad que ha demostrado tener Vinicius Oliveira es algo a lo que hay que prestarle atención. Dado que, aunque sólo lleva 2 peleas en la UFC, fueron suficiente para demostrarnos a los fanáticos de lo que está hecho. Siendo uno de los luchadores más divertidos de ver que existen en estos momentos, cuenta con una taza de finalización altísima, con un récord de 21-3, con 18 de esas 21 victorias siendo por finalización. </p>
+                    <p>Comenzando a narrar un poco su historia, Vinicius “Lok Dog” Oliveira nació el 30 de noviembre de 1995 en Porto Alegre, Brasil. Según <a target='_Blank' href='https://www.ufcespanol.com/athlete/vinicius-oliveira'>explica él mismo a la UFC</a>, comenzó a entrenar debido a que recibía golpizas y palizas constantemente cuando era un niño, cosa que lo motivó a entrenar artes marciales con el objetivo de poder defenderse, y con la idea de poder ganar dinero en el futuro. </p>
+                    <p>Antes de iniciar su carrera profesional en las artes marciales mixtas, Vinicius tuvo que rebuscárselas para ganarse la vida, trabajando en sitios de construcción, de a ratos dedicándose a ser conductor, y vendiendo DVDs en la calle. Además, antes de tener su primera pelea profesional, tuvo alrededor de 30 combates amateur, en donde cosechó 28 victorias y sólo 2 derrotas. </p>
+                    <p>Finalmente, su sueño de volverse profesional se materializaría el 19 de diciembre de 2015, cuando haría su debut ante su compatriota Michael Teixeira, a quien sería capaz de derrotar por TKO en el primer asalto. Consiguiendo un inicio de carrera fantástico con 9 victorias consecutivas en diferentes promotoras brasileñas. </p>
+                    <p>Su primera derrota profesional llegaría recién en el 2018 debido a una parada médica en su pelea contra el mexicano Cristian Quiñonez. </p>
+                    <p>En marzo de 2021 conseguiría ser campeón de la promotora UAE Warriors, luego de conectar una patada en la cabeza y rematar a golpes al entonces campeón canadiense Xavier Alaoui, en el segundo asalto del combate. Sería capaz de defender de manera exitosa su cinturón en octubre de ese mismo año, al someter en el primer asalto a Sylvester Chipfumbu.</p>
+                    <iframe className={styles.article__video} loading='lazy' width="560" height="315" src="https://www.youtube.com/embed/017mV-32V9Q?si=SvN99alPzjXjyA7j" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <p>Finalmente, la oportunidad de su vida llegaría en septiembre de 2023, cuando podría participar del “Dana White’s Contender Series”, en donde peleando por un contrato de UFC, lograría noquear con un gancho de izquierda a Victor Madrigal. </p>
+                    <p>Es entonces cuando el 2 de marzo de 2024, llegaría el punto más alto de su carrera, al protagonizar lo que es en lo personal, una de las mejores peleas que vi en mi vida. Dado que, enfrentando al luchador de Albania, Bernardo Sopaj, iría a una guerra de 3 asaltos, en la que fue dominado completamente durante el primero de estos y la mitad del segundo. Sin embargo, dando la sensación de que Sopaj se cansó para la segunda mitad del segundo asalto, y de que iba a ser imposible que noqueara a Oliveira, el brasileño sacó fuerzas y remontó de manera contundente lo que estaba siendo una pelea que hasta ese momento iba perdiendo. Finalmente, para el tercer asalto, en el que Vinicius hizo de Bernardo un saco de boxeo, consiguió conectar un rodillazo volador a la cabeza que mandó a la lona a su rival de manera instantánea. </p>
+                    <p>No sólo fue épica la pelea, por la forma en que Vinicius Oliveira fue capaz de dar vuelta un combate que estaba perdiendo, sino que también la forma de finalizar dicho combate, con un rodillazo que pocos peleadores de UFC son capaces de conectar, y la forma en que Sopaj cae noqueado instantáneamente, es lo que eleva aquella actuación a otro nivel. Por eso no es de extrañar que la UFC haya recompensado dicha actuación con los 2 bonos, uno por La Pelea de la Noche, y otro por ser la Actuación de la Noche. </p>
+                    <iframe className={styles.article__video} loading='lazy' width="560" height="315" src="https://www.youtube.com/embed/iI5wiSFT1e4?si=lCQx_s1r0XZVL7ab" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <p>En su siguiente pelea, con fecha para junio de ese mismo año, en el <Link href='/covertura-ufc/ufc303'>UFC 303</Link>, se enfrentaría a Ricky Simón, quien es un luchador que cuenta con la particularidad de ser uno de los pocos que puede decir que venció al actual campeón de la división de peso gallo, <Link href='/peleadores/merab-dvalishvili'>Merab Dvalishvili</Link>. Consiguiendo una victoria dominante ante su rival por decisión unánime. Esta pelea tiene doble mérito, y habla de la calidad de persona que es Vinicius, debido a que tuvo que prepararse y entrenar para este combate, al mismo tiempo que se dedicaba a ayudar las victimas de las inundaciones en Brasil, que sufrían durante aquellas fechas. </p>
+                    <EndArticle />
+                </article>
+                <AsideChamps />
+            </section>
+        </main>
+    )
+}
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import ArticlePreview from '../articulos/components/ArticlePreview';
 /* HERO IMAGES */
+import viniciusOliveira from '@/assets/noticias/0-100/30-40/vinicius_oliveira.jpg'
 import predicciones2025 from '@/assets/noticias/0-100/20-30/campeones_ufc2025.jpg'
 import ufcBillonario from '@/assets/noticias/0-100/20-30/ufc_busca_millonada.jpg'
-import iliaTopuriaLigero from '@/assets/noticias/0-100/20-30/ilia_topuria_georgia.jpg'
 import dariushPelea2025 from '@/assets/noticias/0-100/20-30/dariush_pelea2025.jpg'
 import previaUFC311 from '@/assets/noticias/0-100/20-30/previa_ufc311.jpg'
 import entrenamientoProchazka from '@/assets/noticias/0-100/20-30/prochazka_training.png'
@@ -33,8 +33,10 @@ import drakeApuesta from '@/assets/noticias/0-100/10-20/drake_apuesta.jpg'
 import iliaPromissKOHolloway from '@/assets/noticias/0-100/10-20/ilia_promissko_holloway.jpg'
 import michaelMorales from '@/assets/noticias/0-100/10-20/michael_morales.jpg'
 /* 20 - 30 */
+import iliaTopuriaLigero from '@/assets/noticias/0-100/20-30/ilia_topuria_georgia.jpg'
 import evloevPeleaTopuria from '@/assets/noticias/0-100/20-30/evloev_postfight.jpeg'
 import argUFC2024 from '@/assets/noticias/0-100/20-30/argentinos2024.jpg'
+/* 30 - 40 */
 
 
 export default function NoticiasPage () {
@@ -70,13 +72,14 @@ export default function NoticiasPage () {
                 </Link>
             </article>
             <article className={styles.noticiasHero__container}>
+                <ArticlePreview img={viniciusOliveira} url='/noticias/vinicius-oliveira-ufc' title='¿Quién es Vinicius Oliveira? El brasileño con KOs espectaculares en la UFC' author={null} text='La historia y carrera del peleador de UFC brasileño Vinicius Oliveira.' date='29/01/2025' />
                 <ArticlePreview img={entrenamientoProchazka} url='/noticias/como-entrena-jiri-prochazka' title='¿Cómo Entrena Jiri Prochazka? El entrenamiento y preparación mental de Jiri Prochazka que incluye golpear 500 veces un árbol y 3 días encerrado sin luz ni comida para confrontar sus demonios. ' author={null} text='El loco y salvaje régimen de entrenamiento al que se somete el excampeón de peso semipesado, con su famosa preparación mental que incluye pasar 3 días encierrado a oscuras en una habitación con sólo agua. ' date='14/01/2025' />
                 <ArticlePreview img={previaUFC311} url='/noticias/previa-ufc-311' title='Previa UFC 311: Arman Tsarukyan Busca el Título de Peso Ligero en su Revancha Ante Islam Makhachev' author={null} text='¡El equipo Khabib protagonista! En una noche donde los cinturones de peso gallo y peso ligero estarán en juego en el UFC 311, con Merab Dvalishvi haciendo su primer defensa del título ante Umar Nurmagomedov' date='10/01/2025' />
                 <ArticlePreview img={invictosUFC} url='/noticias/peleadores-invictos-ufc' title='Los Peleadores Invictos de UFC en 2025' author={null} text='Los luchadores de UFC que terminaron el 2024 invictos y quienes serán capaces de mantenerlo en el 2025.' date='26/12/2024' />
-                <ArticlePreview img={iliaTopuriaLigero} url='/noticias/ilia-topuria-sube-a-peso-ligero' title='¡Ilia Topuria Abandona la División de Peso Pluma para Pelear en Peso Ligero! ¿Tiene Sentido que Busque Pelear en Peso Ligero Ahora?' author={null} text='El campeón español Ilia Topuria afirma que quizás vimos su última pelea en peso pluma, y que busca pelear en peso ligero contra Charles Oliveira' date='10/12/2024' />
             </article>
             <h2 className={styles.noticiasPage__subtitle}>Todas Las Noticias</h2>
             <article className={styles.noticias}>
+                <ArticlePreview img={iliaTopuriaLigero} url='/noticias/ilia-topuria-sube-a-peso-ligero' title='¡Ilia Topuria Abandona la División de Peso Pluma para Pelear en Peso Ligero! ¿Tiene Sentido que Busque Pelear en Peso Ligero Ahora?' author={null} text='El campeón español Ilia Topuria afirma que quizás vimos su última pelea en peso pluma, y que busca pelear en peso ligero contra Charles Oliveira' date='10/12/2024' />
                 <ArticlePreview img={evloevPeleaTopuria} url='/noticias/evloev-sobre-pelea-con-ilia-topuria' title='Movsar Evloev Cree Poder Ganarle a Ilia Topuria y Pide Pelear por el Cinturón' author={null} text='El luchador ruso Movsar Evloev, dijo tener lo necesario para derrotar al campeón español Ilia Topuria, luego de vencer al estadounidense Aljamain Sterling en el UFC 310' date='28/11/2024' />
                 <ArticlePreview img={espUFC2025} url='/noticias/luchadores-espanoles-en-ufc' title='Los 5 Peleadores Españoles Actuales de UFC en 2024' author={null} text='Los luchadores españoles de UFC que compiten actualmente en la compañía más grande de MMA del mundo.' date='08/12/2024' />
                 <ArticlePreview img={kaiAsakura} url='/noticias/quien-es-kai-asakura' title='¿Quién es Kai Asakura? La Estrella Japonesa Que Debutará Peleando Por el Título de UFC en el UFC 310 en Diciembre' author={null} text='El luchador japonés Kai Asakura de 31 años que peleará por el Campeonato de Peso Mosca en su debut en la UFC.' date='26/11/2024' />
