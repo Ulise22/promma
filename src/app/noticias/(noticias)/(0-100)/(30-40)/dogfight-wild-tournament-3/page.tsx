bdec165fb3f7c221e7fc924e9bb8b74b91d9d0d2
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-01-31 17:00:14 -0300

    second fight done

--- src/app/noticias/(noticias)/(0-100)/(30-40)/dogfight-wild-tournament-3/page.tsx
+++ src/app/noticias/(noticias)/(0-100)/(30-40)/dogfight-wild-tournament-3/page.tsx
@@ -29,8 +29,10 @@ export default function DWT3 () {
                     <iframe className={styles.article__video} loading='lazy' width="560" height="315" src="https://www.youtube.com/embed/tOV2Gy9AQPg?si=qDvtQ-n4CADqdpQL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     <h2>Old School Fight (Capoeira vs Muay Thai)</h2>
                     <p>&quot;La Bomba&quot; consigue una espectacular victoria en representación del Muay Thai ante su rival de Capoeira, Giani, luego de haber dominado por completo la pelea en base a patadas al cuerpo y especialmente a la pierna izquierda de su oponente, que le hicieron mucho daño, llegando al punto en que el peleador de capoeira ya no podía mantenerse de pie. Finalmente, luego de una patada que derribaría a su rival, el representante de Muay Thai consiguió conenctar un golpe en la cabeza que haría que el árbitro parara la pelea, dándole la victoria por TKO en el segundo asalto.</p>
-                    <h2>No Rules Fight</h2>
-                    <p></p>
+                    <h2>No Rules Fight (Yess Castro vs Yamila Sánchez)</h2>
+                    <p>En una pelea donde la mexicana Yess Castro comenzó dominando claramente el combate, conectando patadas a todos el cuerpo de su rival, usando estas patadas para mantener la distancias y mantener lejos a Yamila, Castro fue capaz de dominar todo el combate en las diferentes facetas que tuvo este. </p>
+                    <p>El momento en que la argentina Yamila Sánchez mejor estuvo, fue cuando estuvo cerca de conectar una guillotina que podría haber sometido a su rival. Sin embargo, Yess Castro fue capaz de defender muy bien este intento de sumisión, aprovechando cuando quedó por encima en la lucha en el suelo, para castigar a su rival con ground & pound del que Yamila no pudo escapar. Terminando de esta manera el combate con 8 minutos muy dominantes por parte de la mexicana que fue muy superior a su rival.</p>
+                    <p>De esta manera, la pelea que duró un total de 15 minutos, sin descansos, todo a un solo asaltos, llegó a la mano de los jueces que merecidamente le dieron la victoria a la mexicana Yess Castro en lo que fue un tremenedo combate que tuvo de todo. </p>
                     <h2>Pelea Medieval</h2>
                     <p></p>
                     <h2>2v2 Especial Rules</h2>
