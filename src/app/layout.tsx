937173922ba3731946317ddefa00d956c1b991ca
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-02-02 13:08:00 -0300

    install the google analytics in the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Footer from './components/Footer'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Menu from './components/Menu'
 import Link from 'next/link'
@@ -28,10 +29,12 @@ export default function RootLayout({
         <meta name="google-adsense-account" content="ca-pub-9270621638584149" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9270621638584149"
      crossOrigin="anonymous"></script>
+
       </head>
       <body className={inter.className}>
         <Menu />
         {children}
+        <GoogleAnalytics gaId="G-5FLC9WWZD8" />
         <Analytics />
         <Footer />
       </body>
