9b8a26e96a31a2103052cc6a2625d36329afc7ea
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-03-01 22:40:54 -0300

    ribovics fight done

--- src/app/covertura-ufc/(fight-night)/(0-100)/(0-15)/fight-night-kape-almabayev/page.tsx
+++ src/app/covertura-ufc/(fight-night)/(0-100)/(0-15)/fight-night-kape-almabayev/page.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export default function KapeAlmabayev () {
     return(
         <main>
-            <ArticleHero title='UFC Vegas 103: ¡Manel Kape vs Asu Almabayev!' subtitle='¡Esteban Ribovics vs Nashrat Haqparast! ' image={hero} date='2025-03-01' author={null} updatedDate={null} />
+            <ArticleHero title='UFC Vegas 103: ¡Manel Kape vs Asu Almabayev!' subtitle='¡Esteban Ribovics Cae Derrotado ante Nashrat Haqparast Por Decisión Dividida! ' image={hero} date='2025-03-01' author={null} updatedDate={null} />
             <section className={styles.article__container}>
                 <article className={styles.article}>
                     <p>Tenemos noche de MMA en el UFC APEX en Las Vegas, que será encabezado por el luchador portugués, ranqueado #6 en la división de peso mosca, <b>Manel Kape</b>, quien se enfrentará al luchador de Kazajistán <b>Asu Almabayev</b>, quien aceptó pelear en corto aviso luego de que el rival original de Kape, <b>Brandon Royval</b> se diera de baja pocos días antes del evento. </p>
@@ -32,6 +32,8 @@ export default function KapeAlmabayev () {
                     <h2>¡Hyder Amil Derrota a Williams Gomis por Decisión Dividida en la Pelea de la Noche!</h2>
                     <p>Tremenda pelea la que nos dieron estos 2 guerreros, quienes protagonizaron la que sin dudas es una de las mejores peleas de la noche, con ambos luchadores dando una exhibición, en un combate que podría haberse ido para cualquiera de los dos.</p>
                     <p>Finalmente el combate llegó a mano de los jueces que dieron como ganador a Hydir Amil por decisión dividida, quien mantiene el invicto y extiende su récord profesional a 11-0. </p>
+                    <h2>¡Nasrat Haqparast Derrota a Esteban Ribovics por Decisión Dividida en una auténtica Guerra!</h2>
+                    <p>Si la pelea anterior había parecido una guerra, esto es algo más. En lo que fue un absoluto espectáculo, como lo es toda pelea en la que está el argentino <Link href='/peleadores/esteban-ribovics'>Esteban Ribovics</Link>, vimos un combate bastante cerrado entre ambos, que finalmente se terminó decidiendo en las tarjetas de los jueces, quienes por decisión dividida dieron por ganador al luchador marroquí Nasrat Haqparast. </p>
                     <EndArticle />
                 </article>
                 <AsideChamps />
