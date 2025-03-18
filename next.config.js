758a6771eaa3069158a543dfe9c1389391cf9aaf
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-03-18 20:03:17 -0300

    added the cache to the page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+ async nextConfig() {
+    return [
+        {
+          source: "/(.*).(js|css|png|jpg|jpeg|gif|webp|svg|woff|woff2|ttf|otf|eot|mp4|webm|ogg|avi|mov)$",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=31536000, immutable",
+            },
+          ],
+        },
+        {
+          source: "/(.*).html",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=0, must-revalidate",
+            },
+          ],
+        },
+      ];
+},
+}
+
