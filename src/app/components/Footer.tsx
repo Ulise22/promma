80f3eee854e978ec85ffd2c656c6b50566b70e01
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-07-11 11:52:37 -0300

    made changes in the UI of the navbar and the homepage

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,46 +8,48 @@ import Image from 'next/image'
 export default function Footer () {
     return(
         <footer className={styles.footer}>
-            <Image className={styles.footer__title} src={fullmma} alt='FULLMMA' />
-            <section className={styles.footer__container}>
-                <div className={styles.footer__container__group}>
-                    <h3>Legal</h3>
-                    <ul className={styles.footer__list}>
-                        <li>
-                            <Link className={styles.foter__list__item} href='/cookies'>Política de Cookies</Link>
-                        </li>
-                        <li>
-                            <Link className={styles.foter__list__item} href='/politica-privacidad'>Política de Privacidad</Link>
-                        </li>
-                        <li>
-                            <Link className={styles.foter__list__item} href='/atribuciones-de-imagenes'>Atribuciones de Imagenes</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.footer__container__group}>
-                    <h3>Nosotros</h3>
-                    <ul className={styles.footer__list}>
-                        <li>
-                            <Link className={styles.foter__list__item} href='/contacto'>Contactanos</Link>
-                        </li>
-                        <li>
-                            <Link className={styles.foter__list__item} href='/sobre-nosotros'> Sobre nosotros </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.footer__container__group}>
-                    <h3>Nuestras Redes Sociales</h3>
-                    <ul className={styles.footer__list__social}>
-                        <li>
-                            <a target='_Blank' href='https://x.com/MmaFull265'> <FontAwesomeIcon icon={faSquareXTwitter} size='xl' /> </a>
-                        </li>
-                        <li>
-                            <a target='_Blank' href='https://www.facebook.com/people/Fullmma/61565690940390/'> <FontAwesomeIcon icon={faFacebook} size='xl' /> </a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-            <h4 className={styles.footer__p}>FULLMMA © 2024</h4>
+            <div className={styles.container}>
+                <Image className={styles.footer__title} src={fullmma} alt='FULLMMA' />
+                <section className={styles.footer__container}>
+                    <div className={styles.footer__container__group}>
+                        <h3>Legal</h3>
+                        <ul className={styles.footer__list}>
+                            <li>
+                                <Link className={styles.foter__list__item} href='/cookies'>Política de Cookies</Link>
+                            </li>
+                            <li>
+                                <Link className={styles.foter__list__item} href='/politica-privacidad'>Política de Privacidad</Link>
+                            </li>
+                            <li>
+                                <Link className={styles.foter__list__item} href='/atribuciones-de-imagenes'>Atribuciones de Imagenes</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.footer__container__group}>
+                        <h3>Nosotros</h3>
+                        <ul className={styles.footer__list}>
+                            <li>
+                                <Link className={styles.foter__list__item} href='/contacto'>Contactanos</Link>
+                            </li>
+                            <li>
+                                <Link className={styles.foter__list__item} href='/sobre-nosotros'> Sobre nosotros </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.footer__container__group}>
+                        <h3>Nuestras Redes Sociales</h3>
+                        <ul className={styles.footer__list__social}>
+                            <li>
+                                <a target='_Blank' href='https://x.com/MmaFull265'> <FontAwesomeIcon icon={faSquareXTwitter} size='xl' /> </a>
+                            </li>
+                            <li>
+                                <a target='_Blank' href='https://www.facebook.com/people/Fullmma/61565690940390/'> <FontAwesomeIcon icon={faFacebook} size='xl' /> </a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <h4 className={styles.footer__p}>FULLMMA © 2024</h4>
+            </div>
         </footer>
     )
 }
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -10,24 +10,26 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 export default function Menu () {
     const [isOpen, setIsOpen] = useState(false);
     return(
-        <nav className={styles.menu}>
-            <Link className={styles.menu__logo} href='/'>
-                <Image className={styles.menu__logo__img} priority={true} src={logo} alt='FULLMMA' />
-            </Link>
+        <header className={styles.menu}>
+            <nav className={styles.menu__container}>
+                <Link className={styles.menu__logo} href='/'>
+                    <Image className={styles.menu__logo__img} priority={true} src={logo} alt='FULLMMA' />
+                </Link>
 
-            <div onClick={() => setIsOpen(!isOpen)} className={styles.menu__icon}>
-                { isOpen ? <FontAwesomeIcon icon={faXmark} size='xl' /> : <FontAwesomeIcon icon={faBars} size='xl' /> } 
-            </div>
-            
-            <ul className={isOpen ? `${styles.menu__list} ${styles.active}` : `${styles.menu__list}`}>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/'>Inicio</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/articulos'>Artículos</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/noticias'>Noticias</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/peleadores'>Peleadores</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/leyendas'>Leyendas</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/mujeres'>Mujeres</Link></li>
-                <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/covertura-ufc'>UFC</Link></li>
-            </ul>
-        </nav>
+                <div onClick={() => setIsOpen(!isOpen)} className={styles.menu__icon}>
+                    { isOpen ? <FontAwesomeIcon icon={faXmark} size='xl' /> : <FontAwesomeIcon icon={faBars} size='xl' /> } 
+                </div>
+                
+                <ul className={isOpen ? `${styles.menu__list} ${styles.active}` : `${styles.menu__list}`}>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/'>Inicio</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/articulos'>Artículos</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/noticias'>Noticias</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/peleadores'>Peleadores</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/leyendas'>Leyendas</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/mujeres'>Mujeres</Link></li>
+                    <li onClick={() => setIsOpen(false)} className={styles.menu__list__item}><Link className={styles.menu__list__item__link} href='/covertura-ufc'>UFC</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
