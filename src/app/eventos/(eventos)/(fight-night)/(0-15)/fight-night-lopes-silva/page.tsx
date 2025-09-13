8d8b176986f9809dd6f447ccec04129fb732abd4
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-09-13 16:23:39 -0300

    first fight of the UFC noche done

---
+++ src/app/eventos/(eventos)/(fight-night)/(0-15)/fight-night-lopes-silva/page.tsx
@@ -0,0 +1,58 @@
+import styles from '@/app/eventos/components/articleEvents.module.css'
+import ArticleHero from '@/app/articulos/components/ArticleHero'
+import Link from 'next/link'
+import type { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+/* Images */
+import hero from '@/assets/eventos/fight-night/0-150/0-15/lopes-vs-silva.webp'
+import desktopAd from '@/app/eventos/components/assets/desktop.gif'
+import mobileAd from '@/app/eventos/components/assets/mobileAd.gif'
+import secondAd from '@/app/eventos/components/assets/Media10417__es300х250.gif'
+import Image from 'next/image'
+
+const AsideChamps = dynamic(() => import('@/app/components/asides/AsideChamps'))
+const EndArticle = dynamic(() => import('@/app/components/EndArticle'))
+const ArticlesFooter = dynamic(() => import('@/app/components/recomendedArticles/ArticlesFooter'))
+
+export const metadata: Metadata = {
+    title: 'UFC Noche: ¡Diego Lopes vs Jean Silva!',
+    description: '',
+    openGraph: {
+        images: '',
+        title: 'UFC Noche: ¡Diego Lopes vs Jean Silva!',
+        description: '',
+        url: 'https://fullmma.org/eventos/fight-night-lopes-silva'
+    }
+}
+
+export default function ImavovBorralho () {
+    return(
+        <main>
+            <ArticleHero title='UFC Noche: ¡Diego Lopes vs Jean Silva!' subtitle='¡David Martínez vs Rob Font!' image={hero} date='2025-09-13' author={null} updatedDate={null} />
+            <section className={styles.article__container}>
+                <article className={styles.article}>
+                    <span className={styles.article__warning}>‼️Las apuestas están prohibidas para menores de edad. Si crees que tienes un problema con el juego, busca ayuda profesional. Jugar debe ser una actividad de entretenimiento, no una forma de ganar dinero.</span>
+                    <Link className={styles.article__ads} target='_Blank' rel='sponsored' href='https://refpa58144.com/L?tag=d_4624339m_1599c_&site=4624339&ad=1599'>
+                        <Image className={styles.desktopAd} src={desktopAd} alt='' />
+                        <Image className={styles.mobileAd} src={mobileAd} alt='' />
+                    </Link>
+                    <p>Tremendo evento de UFC tenemos el día de hoy en honor a la Independencia de México, donde el luchador de doble nacionalidad brasileña/mexicana <Link href='/peleadores/diego-lopes'>Diego Lopes</Link>, quien estará enfrentando a su compatriota de Brasil <b>Jean Silva</b>, quien actualmente es el único luchador invicto en la UFC de los <Link href='/articulos/fighting-nerds'>Fighting Nerds</Link>. </p>
+                    <p>Además, en la pelea coestelar estarán peleando el #9 de la división de peso gallo de la compañía <b>Rob Font</b>, quien defenderá su puesto en los rankings ante el prospecto mexicano <b>David Martínez</b>, quien en su segunda pelea en la UFC ya tendrá una prueba de fuego. Sin mencionar la enorme cantidad de peleadores mexicano y latinos que estarán presentes peleando el día de hoy. </p>
+                    <p></p>
+                    <h2 className={styles.article__fightsHierarchy}>Cartelera Preliminar</h2>
+                    <h2>Daniil Donchenko Noquea a Rodrigo Sezinando en el Primer Asalto en al Final de The Ultimate Fighter</h2>
+                    <p>Tremenda manera de comenzar esta cartelera del Noche UFC, que nos trajó la final del TUF en peso wélter, donde el ucraniano <b>Daniil Donchenko</b> fue capaz de imponerse por sobre su rival <b>Rodrigo Sezinando</b>, conectando un montón de codazos a la cara de su rival que le generaron cortes en el rostro, convirtiendo esto en una pelea sangrienta. Finalmente fue una mano derecha la que consiguió el knockdown, que llegó seguido de una avalancha de golpes que el árbitro los separará y diera como ganador por TKO al luchador ucraniano. </p>
+                    {/* <h2 className={styles.article__fightsHierarchy}>Cartelera Estelar</h2>
+                    <Link className={styles.article__ads} target='_Blank' rel='sponsored' href='https://refpa58144.com/L?tag=d_4624339m_1599c_&site=4624339&ad=1599'>
+                        <Image src={secondAd} alt='' />
+                    </Link>
+                    <h2></h2>
+                    <p> </p> */}
+                    <EndArticle />
+                </article>
+                <AsideChamps />
+            </section>
+            <ArticlesFooter />
+        </main>
+    )
+}
--- src/app/eventos/components/PageHero.tsx
+++ src/app/eventos/components/PageHero.tsx
@@ -3,8 +3,8 @@ import styles from './PageHero.module.css'
 import Image from 'next/image'
 /* IMAGES */
 import ufc319 from '@/assets/eventos/ufc/0-150/0-15/ufc319.webp'
-import walkerVSmingyang from '@/assets/eventos/fight-night/0-150/0-15/walker_vs_mingyang.webp'
 import imavovBorralho from '@/assets/eventos/fight-night/0-150/0-15/imavov_vs_borralho.webp'
+import lopesSilva from '@/assets/eventos/fight-night/0-150/0-15/lopes-vs-silva.webp'
 
 export default function PageHero () {
     return(
@@ -14,7 +14,7 @@ export default function PageHero () {
                 <h2 className={styles.page__title}>UFC 319: ¡Khamzat Chimaev Derrota a Dricus Du Plessis y es Nuevo Campeón de Peso Medio! ¡Lerone Murphy Deja KO al Debutante de UFC Aaron Pico!</h2>
                 <p>¡Loopy Godínez Derrota a Jéssica Andrade por Decisión Unánime en un Peleón! ¡Carlos Prates Noquea a Geoff Neal en el Primer Asalto con un Codazo Giratorio!</p>
             </Link>
-            <Link className={`${styles.pagehero__article} ${styles.normal1}`} href='/eventos/fight-night-imavov-borralho'>
+            <Link className={`${styles.pagehero__article} ${styles.normal2}`} href='/eventos/fight-night-imavov-borralho'>
                 <Image className={styles.pagehero__article__img} quality={50} src={imavovBorralho} alt='' />
                 <article className={styles.pagehero__article__container}>
                     <h2 className={styles.pagehero__title}>UFC Paris: ¡Nassourdine Imavov Derrota a Caio Borralho Por Decisión Unánime en La Pelea de la Noche!</h2>
@@ -25,14 +25,14 @@ export default function PageHero () {
                     </div>
                 </article>
             </Link>
-            <Link className={`${styles.pagehero__article} ${styles.normal2}`} href='/eventos/fight-night-walker-mingyang'>
-                <Image className={styles.pagehero__article__img} quality={50} src={walkerVSmingyang} alt='' />
+            <Link className={`${styles.pagehero__article} ${styles.normal1}`} href='/eventos/fight-night-lopes-silva'>
+                <Image className={styles.pagehero__article__img} quality={50} src={lopesSilva} alt='' />
                 <article className={styles.pagehero__article__container}>
-                    <h2 className={styles.pagehero__title}>UFC Shanghai: ¡Johnny Walker Noquea a Zhang Mingyang en el Segundo Asalto en China!</h2>
-                    <p>¡Aljamain Sterling Derrota Por Decisión Unánime a Brian Ortega 50-45! ¡Sergei Pavlovich Derrota a Waldo Cortes Acosta Por Decisión Unánime!</p>
+                    <h2 className={styles.pagehero__title}>UFC Noche: ¡Diego Lopes vs Jean Silva!</h2>
+                    <p>¡David Martínez vs Rob Font!</p>
                     <div className={styles.pagehero__article__info}>
                         <h4>FULLMMA</h4>
-                        <p>23/08/2025</p>
+                        <p>13/09/2025</p>
                     </div>
                 </article>
             </Link>
