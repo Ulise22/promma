d6e43231fcebdc39ea5da656fee8b299058dd508
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-09-29 18:35:15 -0300

    changed the homepage articles to be more actual

--- src/app/components/homepage/HomeHero.tsx
+++ src/app/components/homepage/HomeHero.tsx
@@ -2,17 +2,17 @@ import Link from 'next/link'
 import styles from './homeHero.module.css'
 import Image from 'next/image'
 /* IMAGES */
-import chimaevEnfermedad from '@/assets/articulos/0-100/60-70/khamzat_chimaev_enfermedad.webp'
+import alexPereiraTribu from '@/assets/articulos/0-100/70-80/alex_pereira-tribu.webp'
 import chimaevCicatriz from '@/assets/articulos/0-100/60-70/chimaev_cicatriz.webp'
-import modeloUFC from '@/assets/noticias/0-100/30-40/modelo_ufc.webp'
+import merabCardio from '@/assets/articulos/0-100/70-80/merab_dvalishvili-cardio.webp'
 
 export default function HomeHero () {
     return(
         <section className={styles.homehero}>
-            <Link className={styles.homehero__bigArticle} href='/articulos/que-enfermedad-tiene-khamzat-chimaev'>
-                <Image className={styles.homehero__bigArticle__img} loading='eager' quality={75} src={chimaevEnfermedad} alt='Khamzat Chimaev en una camilla de un hospital generado por IA' />
-                <h2 className={styles.home__title}>¿Qué Enfermedad Tiene Khamzat Chimaev? Los Problemas de Salud de la Estrella de la UFC</h2>
-                <span>Todos los problemas de salud que sufrió Khamzat Chimaev que le impidieron pelear seguido y como se sobrepuso a ellos.</span>
+            <Link className={styles.homehero__bigArticle} href='/articulos/alex-pereira-tribu'>
+                <Image className={styles.homehero__bigArticle__img} loading='eager' quality={75} src={alexPereiraTribu} alt='Alex Pereira Pintado Como Si Formara Parte de una Tribu Indigena' />
+                <h2 className={styles.home__title}>¿Cuál es la Tribu de Alex Pereira?</h2>
+                <span>El origen de Alex Pereira y su relación la tribu indigena brasileña Pataxó, de la que es descendiente</span>
             </Link>
             <Link className={`${styles.homehero__article} ${styles.normal1}`} href='/articulos/que-le-paso-en-el-labio-a-khamzat-chimaev'>
                 <Image className={styles.homehero__article__img} quality={50} src={chimaevCicatriz} alt='' />
@@ -25,14 +25,14 @@ export default function HomeHero () {
                     </div>
                 </article>
             </Link>
-            <Link className={`${styles.homehero__article} ${styles.normal2}`} href='/noticias/modelo-de-ufc'>
-                <Image className={styles.homehero__article__img} quality={50} src={modeloUFC} alt='Islam Dulatov, el modelo de UFC' />
+            <Link className={`${styles.homehero__article} ${styles.normal2}`} href='/articulos/merab-dvalishvili-cardio'>
+                <Image className={styles.homehero__article__img} quality={50} src={merabCardio} alt='Islam Dulatov, el modelo de UFC' />
                 <article className={styles.homehero__article__container}>
-                    <h2 className={styles.homehero__title}>Islam Dulatov: El Modelo de UFC Que Debuta en el UFC 318</h2>
-                    <p>Conoce al alemán Islam Dulatov, que renunció a su exitosa carrera de modelo de Gucci y Hugo Boss para convertirse en peleador de MMA y debutar en la UFC.</p>
+                    <h2 className={styles.homehero__title}>El Cardio de Merab Dvalishvili</h2>
+                    <p>¿Cómo Conseguir el Cardio de Merab Dvalishvili? El Secreto de Merab Para Tener un Cardio Inhumano.</p>
                     <div className={styles.homehero__article__info}>
                         <h4>FULLMMA</h4>
-                        <p>17/07/2025</p>
+                        <p>18/09/2025</p>
                     </div>
                 </article>
             </Link>
