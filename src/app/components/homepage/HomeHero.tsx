32c8dcd1a913d112e9433fce67d0eeda92a9c07c
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-10-31 09:37:44 -0300

    updated articles in the homepage

--- src/app/components/homepage/HomeHero.tsx
+++ src/app/components/homepage/HomeHero.tsx
@@ -2,37 +2,37 @@ import Link from 'next/link'
 import styles from './homeHero.module.css'
 import Image from 'next/image'
 /* IMAGES */
+import dellaMaddalenaNariz from '@/assets/articulos/0-100/70-80/jack-della-maddalena-nariz.webp'
 import alexPereiraTribu from '@/assets/articulos/0-100/70-80/alex_pereira-tribu.webp'
-import chimaevCicatriz from '@/assets/articulos/0-100/60-70/chimaev_cicatriz.webp'
-import merabCardio from '@/assets/articulos/0-100/70-80/merab_dvalishvili-cardio.webp'
+import makhachevDerrota from '@/assets/articulos/0-100/70-80/makhachev-derrota.webp'
 
 export default function HomeHero () {
     return(
         <section className={styles.homehero}>
-            <Link className={styles.homehero__bigArticle} href='/articulos/alex-pereira-tribu'>
-                <Image className={styles.homehero__bigArticle__img} loading='eager' quality={75} src={alexPereiraTribu} alt='Alex Pereira Pintado Como Si Formara Parte de una Tribu Indigena' />
-                <h2 className={styles.home__title}>¿Cuál es la Tribu de Alex Pereira?</h2>
-                <span>El origen de Alex Pereira y su relación la tribu indigena brasileña Pataxó, de la que es descendiente</span>
+            <Link className={styles.homehero__bigArticle} href='/articulos/jack-della-maddalena-nariz'>
+                <Image className={styles.homehero__bigArticle__img} loading='eager' quality={75} src={dellaMaddalenaNariz} alt='' />
+                <h2 className={styles.home__title}>¿Qué Le Pasó a Jack Della Maddalena en la Nariz?</h2>
+                <span>¿Qué le pasó en la nariz a Jack Della Maddalena? Como un rodillazo en la cabeza le deformó el tabique al campeón de la UFC.</span>
             </Link>
-            <Link className={`${styles.homehero__article} ${styles.normal1}`} href='/articulos/que-le-paso-en-el-labio-a-khamzat-chimaev'>
-                <Image className={styles.homehero__article__img} quality={50} src={chimaevCicatriz} alt='' />
+            <Link className={`${styles.homehero__article} ${styles.normal1}`} href='/articulos/islam-makhachev-unica-derrota'>
+                <Image className={styles.homehero__article__img} quality={50} src={makhachevDerrota} alt='' />
                 <article className={styles.homehero__article__container}>
-                    <h2 className={styles.homehero__title}>Qué le Pasó en el Labio a Khamzat Chimaev</h2>
-                    <p>Descubre la historia detrás la característica cicatríz que tiene la estrella de la UFC Khamzat Chimaev.</p>
+                    <h2 className={styles.homehero__title}>La Única Derrota de Islam Makhachev</h2>
+                    <p>Primer derrota profesional en las MMA del luchador de Daguestán Islam Makhachev. </p>
                     <div className={styles.homehero__article__info}>
                         <h4>FULLMMA</h4>
-                        <p>07/08/2025</p>
+                        <p>10/09/2025</p>
                     </div>
                 </article>
             </Link>
-            <Link className={`${styles.homehero__article} ${styles.normal2}`} href='/articulos/merab-dvalishvili-cardio'>
-                <Image className={styles.homehero__article__img} quality={50} src={merabCardio} alt='Islam Dulatov, el modelo de UFC' />
+            <Link className={`${styles.homehero__article} ${styles.normal2}`} href='/articulos/alex-pereira-tribu'>
+                <Image className={styles.homehero__article__img} quality={50} src={alexPereiraTribu} alt='Alex Pereira Pintado Como Si Formara Parte de una Tribu Indigena' />
                 <article className={styles.homehero__article__container}>
-                    <h2 className={styles.homehero__title}>El Cardio de Merab Dvalishvili</h2>
-                    <p>¿Cómo Conseguir el Cardio de Merab Dvalishvili? El Secreto de Merab Para Tener un Cardio Inhumano.</p>
+                    <h2 className={styles.homehero__title}>¿Cuál es la Tribu de Alex Pereira?</h2>
+                    <p>El origen de Alex Pereira y su relación la tribu indigena brasileña Pataxó, de la que es descendiente</p>
                     <div className={styles.homehero__article__info}>
                         <h4>FULLMMA</h4>
-                        <p>18/09/2025</p>
+                        <p>09/09/2025</p>
                     </div>
                 </article>
             </Link>
