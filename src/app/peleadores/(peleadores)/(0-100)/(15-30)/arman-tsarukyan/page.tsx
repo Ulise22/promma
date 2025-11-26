c2ae360399e7b2e17e15de4538f163e9b2940536
Ulises Montenegro <ulimontenegro23@gmail.com>
2025-11-25 21:31:55 -0300

    updated records after ufc qatar

--- src/app/peleadores/(peleadores)/(0-100)/(15-30)/arman-tsarukyan/page.tsx
+++ src/app/peleadores/(peleadores)/(0-100)/(15-30)/arman-tsarukyan/page.tsx
@@ -21,14 +21,14 @@ export const metadata: Metadata = {
 export default function Tsarukyan () {
     return(
         <main>
-            <PeleadoresHero peleadoresImage={tsarukyan} w={22} l={3} d={0} nombre='Arman Tsarukyan' apodo='AHALKALAKETS' categoria='Peso Ligero' time='2025-10-29' />
+            <PeleadoresHero peleadoresImage={tsarukyan} w={23} l={3} d={0} nombre='Arman Tsarukyan' apodo='AHALKALAKETS' categoria='Peso Ligero' time='2025-10-29' />
             <section className={styles.peleador__articles__container}>
                 <article className={styles.peleador__article}>
                     <h2 className={styles.peleador__article__title}>Arman Tsarukyan UFC</h2>
                     <p className={styles.peleador__article__text}>Arman Tsarukyan en peleador de nacionalidad armenia y rusa, que compite en la división de peso ligero de la UFC, desde que hiciera su debut contra el ruso <Link href='/peleadores/islam-makhachev'>Islam Makhachev</Link> el 20 de abril de 2019, quien luego de 3 asaltos lo derrotaría por decisión unánime. </p>
                     <p className={styles.peleador__article__text}>Actualmente es de los peleadores más importantes en la división de peso ligero en la UFC, teniendo en su haber victorias tan meritorias como la obtenida ante el excampeón de la división <Link href='/peleadores/charles-oliveira'>Charles Oliveira</Link>, o contra <b>Beneil Dariush</b>, que lo posicionan como uno de los mejores luchadores de las 155lbs en estos momentos. </p>
                     <h2 className={styles.peleador__article__title}>Arman Tsarukyan Récord</h2>
-                    <p className={styles.peleador__article__text}>Arman Tsarukyan posee un récord de 22-3. Siendo 9 de sus 21 victorias por la vía del nocaut, 5 por la vía de la sumisión y 8 por decisión de los jueces. Mientras que de sus 3 derrotas, 2 han sido por decisión y sólo 1 por nocaut al inicio de su carrera en 2015.</p>
+                    <p className={styles.peleador__article__text}>Arman Tsarukyan posee un récord de 23-3. Siendo 9 de sus 21 victorias por la vía del nocaut, 6 por la vía de la sumisión y 8 por decisión de los jueces. Mientras que de sus 3 derrotas, 2 han sido por decisión y sólo 1 por nocaut al inicio de su carrera en 2015.</p>
                     <h2 className={styles.peleador__article__title}>Arman Tsarukyan Historia</h2>
                     <p className={styles.peleador__article__text}>Arman Tsarukyan nació el 11 de octubre de 1996 en Ajalkalaki, un pequeño pueblo de Georgia cercano a la frontera con Armenia. Es el hermano del medio de 3, teniendo un hermano mayor llamado Artur y una hermana menor llamada Isabella, y es el hijo de un empresario inmobiliario llamado Nairi Tsarukyan, con quienes a la edad de 3 años se mudó a Rusia en busca de mejorar su calidad de vida.</p>
                     <iframe className={styles.peleador__article__video} loading='lazy' width="560" height="315" src="https://www.youtube.com/embed/OuVDFYjRslA?si=WDeaWyQVFSjHYtqV" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
