7545cb57c1f7dd5d17ddc1414640cbae132511f3
Ulises Montenegro <ulimontenegro23@gmail.com>
2026-05-09 19:37:18 -0300

    third fight done

--- src/app/eventos/(eventos)/(ufc)/(0-15)/ufc328/page.tsx
+++ src/app/eventos/(eventos)/(ufc)/(0-15)/ufc328/page.tsx
@@ -12,11 +12,11 @@ const ArticlesFooter = dynamic(() => import('@/app/components/recomendedArticles
 
 export const metadata: Metadata = {
     title: 'UFC 328: ¡Khamzat Chimaev vs Sean Strickland!',
-    description: "",
+    description: "UFC 328 con Khamzat Chimaev defendiendo el título ante Sean Strickland. Además, el japonés Tatsuro Taira busca quitarle el cinturón de peso mosca a Joshua Van. ",
     openGraph: {
         images: 'https://fullmma.org/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fufc328.8c6f1437.webp&w=828&q=65',
         title: 'UFC 328: ¡Khamzat Chimaev vs Sean Strickland!',
-        description: "",
+        description: "UFC 328 con Khamzat Chimaev defendiendo el título ante Sean Strickland. Además, el japonés Tatsuro Taira busca quitarle el cinturón de peso mosca a Joshua Van. ",
         url: 'https://fullmma.org/eventos/ufc328'
     }
 }
@@ -34,6 +34,8 @@ export default function UFC328 () {
                     <p>En un dominio total por parte del luchador peruano <b>José Ochoa</b>, el latinoamericano finalmente se lleva la victoria luego de mostrar un striking muchos niveles superior al de su rival estadounidense <b>Clayton Carpenter</b>, quien no sólo no pudo ante el striking de su rival, sino que tampoco pudo llevarlo al suelo, donde podría haber tenido alguna ventaja. De esta manera, el luchador de Perú se lleva la victoria habiendo ganado todos los asaltos.</p>
                     <h2>Baisangur Susurkaev Somete a Djorden Santos en el tercer asalto</h2>
                     <p>Espectacular desempeño del checheno <b>Baisangur Susurkaev</b>, que en una pelea competetitva, más cerrada de lo que esperabamos, con buenas combinaciones de ambos luchadores, es capaz de finalizarla en el último asalto, conectando un mataleón espectacular que terminaría desmayando a su rival brasileño <b>Djorden Santos</b>. </p>
+                    <h2>Pat Sabatini Derrota a William Gomis por Decisión Unánime</h2>
+                    <p>Gran victoria para el estadounidense <b>Pat Sabatini</b>, quien mediante un dominio total de la lucha fue capaz de ganar sumando puntos ante el francés <b>William Gomis</b>, quien aunque resistió bastante sin ser derribado, no fue capaz de hacerle daño alguno a su rival durante gran parte del combate. De esta manera, el estadounidense se lleva la victoria por decisión unánime. </p>
                     {/* <h2 className={styles.article__fightsHierarchy}>Preliminares</h2>
                     <h2></h2>
                     <p></p> */}
